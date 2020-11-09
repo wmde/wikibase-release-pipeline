01a73274c231e23aba83c2d17626d2fea47061f0
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-11-09 16:52:08 +0100

    Add env vars

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ git clone --depth 1 --branch REL1_35 https://gerrit.wikimedia.org/r/mediawiki/sk
 
 ROOT=`pwd`
 WIKIBASE_PATH="$ROOT/Wikibase"
-
+ENV_FILE="$ROOT/wikibase.env"
 chmod a+rw -R core
-docker run -v "$ROOT/core:/workspace/src" -v "$WIKIBASE_PATH:/workspace/src/extensions/Wikibase" -v "$ROOT/Vector:/workspace/src/skins/Vector/" docker-registry.wikimedia.org/releng/quibble-stretch-php73 --packages-source composer --db sqlite --skip-zuul
+docker run --env-file "$ENV_FILE"  -v "$ROOT/core:/workspace/src" -v "$WIKIBASE_PATH:/workspace/src/extensions/Wikibase" -v "$ROOT/Vector:/workspace/src/skins/Vector/" docker-registry.wikimedia.org/releng/quibble-stretch-php73 mediawiki/extensions/Wikibase --packages-source composer --db sqlite --skip-zuul
