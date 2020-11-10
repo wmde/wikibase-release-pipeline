0d99c16ecdf2ba5af43e5cd725b7e288319ce3d2
Marius Hoch <hoo@online.de>
2020-11-10 16:13:04 +0100

    Workflows: Cache needed extensions

--- update_cache.sh
+++ update_cache.sh
@@ -2,6 +2,8 @@
 
 cd "$(dirname "$0")"
 
+extensions=("cldr" "UniversalLanguageSelector" "Wikibase" "WikibaseCirrusSearch")
+
 # Initial clones, if needed
 mkdir -p git_cache/mediawiki/skins
 if [ ! -d git_cache/mediawiki/core.git ]; then
@@ -13,6 +15,15 @@ fi
 if [ ! -d git_cache/mediawiki/skins/Vector.git ]; then
 	git clone --bare https://gerrit.wikimedia.org/r/mediawiki/skins/Vector git_cache/mediawiki/skins/Vector.git
 fi
+
+for extension in "${extensions[@]}"; do
+	if [ ! -d "git_cache/mediawiki/extensions/$extension.git" ]; then
+		git clone --bare "https://gerrit.wikimedia.org/r/mediawiki/extensions/$extension.git" "git_cache/mediawiki/extensions/$extension.git"
+	fi
+	cd "git_cache/mediawiki/extensions/$extension.git"
+	git fetch --all
+	cd -
+done
 
 cd git_cache/mediawiki/core.git
 git fetch --all
