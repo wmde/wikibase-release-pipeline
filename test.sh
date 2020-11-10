e1efb893f9c1360c73ef5ec56daba973398a4c11
Marius Hoch <hoo@online.de>
2020-11-10 12:52:56 +0100

    test.sh: Formatting, use --skip all, disable SELinux labeling

--- test.sh
+++ test.sh
@@ -16,4 +16,15 @@ chmod a+rw -R git_cache
 
 #docker run --env-file "$ENV_FILE"  -v "$ROOT/core:/workspace/src" -v "$WIKIBASE_PATH:/workspace/src/extensions/Wikibase" -v "$ROOT/Vector:/workspace/src/skins/Vector/" docker-registry.wikimedia.org/releng/quibble-stretch-php73 mediawiki/extensions/Wikibase --packages-source composer --db sqlite --skip-zuul --skip selenium,npm-test,phpunit-standalone,api-testing ----project-branch REL1_35
 
-docker run --env-file "$ENV_FILE"  -v "$ROOT/git_cache:/srv/git" docker-registry.wikimedia.org/releng/quibble-stretch-php73 --packages-source composer --db sqlite --skip selenium,npm-test,phpunit-standalone,api-testing --project-branch mediawiki/core="$BRANCH" --project-branch mediawiki/extensions/UniversalLanguageSelector="$BRANCH" --project-branch mediawiki/extensions/WikibaseCirrusSearch="$BRANCH" --project-branch mediawiki/extensions/cldr="$BRANCH" mediawiki/extensions/UniversalLanguageSelector mediawiki/extensions/WikibaseCirrusSearch mediawiki/extensions/cldr
+docker run --env-file "$ENV_FILE" \
+	-v "$ROOT/git_cache:/srv/git:ro" \
+	--security-opt label=disable \
+	docker-registry.wikimedia.org/releng/quibble-stretch-php73 \
+	--packages-source composer \
+	--db sqlite \
+	--skip all \
+	--project-branch mediawiki/core="$BRANCH" \
+	--project-branch mediawiki/extensions/UniversalLanguageSelector="$BRANCH" \
+	--project-branch mediawiki/extensions/WikibaseCirrusSearch="$BRANCH" \
+	--project-branch mediawiki/extensions/cldr="$BRANCH" \
+	mediawiki/extensions/UniversalLanguageSelector mediawiki/extensions/WikibaseCirrusSearch mediawiki/extensions/cldr
