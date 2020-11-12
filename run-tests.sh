810a1c97c6f9420b7b95b50c2e59d96777184046
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-11-12 17:23:03 +0100

    remove silly status-code thing

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-STATUSCODE=$(curl --fail --show-error --output /dev/null --silent $WIKIBASE_SERVER/wiki/Main_Page)
-exit $STATUSCODE
+curl --fail --show-error --output /dev/null --silent $WIKIBASE_SERVER/wiki/Main_Page
