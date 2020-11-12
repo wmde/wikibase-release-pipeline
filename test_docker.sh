f6b1f2725e44b94cd2d0857dced12765db926dd2
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-11-12 16:54:16 +0100

    Fix docker artifact name

--- test_docker.sh
+++ test_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker load -i "$WIKIBASE_IMAGE_NAME.tar.gz"
+docker load -i "$WIKIBASE_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
 docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh 
