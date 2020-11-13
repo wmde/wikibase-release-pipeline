a7c98f14bb02e43a502da74c614de375ecfc1d06
Marius Hoch <hoo@online.de>
2020-11-13 12:03:35 +0100

    Fix query service building

--- build_scripts/build_queryservice_docker.sh
+++ build_scripts/build_queryservice_docker.sh
@@ -2,6 +2,6 @@
 SERVICE_DIST_TAR="$TARBALL_PATH"
 
 mv "$SERVICE_DIST_TAR" DockerQueryService
-docker build --build-arg tarball="$SERVICE_DIST_TAR" DockerQueryService/ -t "$QUERYSERVICE_IMAGE_NAME"
+docker build --build-arg tarball="$(basename "$SERVICE_DIST_TAR")" DockerQueryService/ -t "$QUERYSERVICE_IMAGE_NAME"
 
 docker save "$QUERYSERVICE_IMAGE_NAME" | gzip -9f > "$QUERYSERVICE_IMAGE_NAME".docker.tar.gz
