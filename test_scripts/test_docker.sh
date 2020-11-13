9ce6a0b8592b2591e295c6d28eaeea0c88ac4a52
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-11-13 15:59:12 +0100

    Update test script path

--- test_scripts/test_docker.sh
+++ test_scripts/test_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd test_scripts
 docker load -i "$WIKIBASE_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
 docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh 
