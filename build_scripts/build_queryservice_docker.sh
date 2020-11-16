64edb5d08aac41a00ad49d3e38cdbd93c491ea10
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 13:21:10 +0000

    build wikibase from git not quibble

--- build_scripts/build_queryservice_docker.sh
+++ build_scripts/build_queryservice_docker.sh
@@ -2,6 +2,6 @@
 SERVICE_DIST_TAR="$TARBALL_PATH"
 
 mv "$SERVICE_DIST_TAR" DockerQueryService
-docker build --build-arg tarball="$(basename "$SERVICE_DIST_TAR")" DockerQueryService/ -t "$QUERYSERVICE_IMAGE_NAME"
+docker build --build-arg tarball="$(basename "$SERVICE_DIST_TAR")" DockerQueryService/ -t "$1"
 
-docker save "$QUERYSERVICE_IMAGE_NAME" | gzip -9f > "$QUERYSERVICE_IMAGE_NAME".docker.tar.gz
+docker save "$1" | gzip -9f > "$1".docker.tar.gz
--- build_scripts/build_wikibase.sh
+++ build_scripts/build_wikibase.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
-docker pull docker-registry.wikimedia.org/releng/quibble-stretch-php73:latest
 
 ROOT="$(pwd)"
 BRANCH="REL1_35"
-ENV_FILE="$ROOT/wikibase.env"
-TEMP_DIR="$(mktemp -d)"
 
-# Remove previous container id file
-rm container_id -f
+TEMP_GIT_DIR="$(mktemp -d)"
+TEMP_TAR_DIR="$(mktemp -d)"
 
-mkdir cache -p
-
-chmod a+rw -R --silent git_cache cache
-
-docker run --env-file "$ENV_FILE" \
-	--cidfile "$ROOT/container_id" \
-	-v "$ROOT"/cache:/cache \
-	-v "$ROOT"/git_cache:/srv/git:ro \
-	--security-opt label=disable \
-	docker-registry.wikimedia.org/releng/quibble-stretch-php73 \
-	--packages-source composer \
-	--db sqlite \
-	--git-cache /srv/git \
-	--skip all \
-	--skip-install
-
-# copy cloned repo and remove container
-CONTAINER_ID=`cat container_id`
-docker cp "$CONTAINER_ID":/workspace/src/extensions/Wikibase /tmp/
-docker rm -f "$CONTAINER_ID"
+git clone --depth 1 --single-branch --branch ${BRANCH} "$ROOT/git_cache/mediawiki/extensions/Wikibase.git" "$TEMP_GIT_DIR"
+git -C "$TEMP_GIT_DIR/Wikibase" submodule update
 
 # remove git things from release package
-rm /tmp/Wikibase/.git* -rfv
+rm "$TEMP_GIT_DIR/Wikibase".git* -rfv
 
-GZIP=-9 tar -C /tmp -zcvf "$TEMP_DIR"/Wikibase.tar.gz Wikibase
+GZIP=-9 tar -C "$TEMP_GIT_DIR" -zcvf "$TEMP_TAR_DIR"/Wikibase.tar.gz Wikibase
 
-echo "TARBALL_PATH="$TEMP_DIR/Wikibase.tar.gz"" >> $GITHUB_ENV
+echo "TARBALL_PATH="$TEMP_TAR_DIR/Wikibase.tar.gz"" >> $GITHUB_ENV
