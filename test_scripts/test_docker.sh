2a78b706b4334b32722570a7ba31e84634bf6876
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 14:30:22 +0000

    ensure test container is built

--- test_scripts/test_docker.sh
+++ test_scripts/test_docker.sh
@@ -3,4 +3,4 @@
 cd test_scripts
 docker load -i "../$WIKIBASE_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
-docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh 
+docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh --build
