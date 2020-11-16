3ee197aa0de3dc592b05bdc9e360e084f8a32df0
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 14:38:51 +0000

    Add curl test for wdqs

--- test_scripts/run-tests.sh
+++ test_scripts/run-tests.sh
@@ -8,7 +8,16 @@ else
     exit 1
 fi
 
+
 # QueryService test
+if curl --fail --show-error --output /dev/null --silent $QUERYSERVICE_SERVER/; then
+    echo 'Successfully loaded the root page!'
+else
+    echo "Could not retrieve root page"
+    exit 1
+fi
+
+# QueryService UI test
 if curl --fail --show-error --output /dev/null --silent $QUERYSERVICE_UI_SERVER/; then
     echo 'Successfully loaded the UI root page!'
 else
