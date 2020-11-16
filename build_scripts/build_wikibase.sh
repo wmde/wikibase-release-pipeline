ddd1bb96c9567b5ca593d0f0c9b1f33e028cc7ee
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 15:33:37 +0000

    Fix tar artifact

--- build_scripts/build_wikibase.sh
+++ build_scripts/build_wikibase.sh
@@ -7,12 +7,12 @@ BRANCH="REL1_35"
 TEMP_GIT_DIR="$(mktemp -d)"
 TEMP_TAR_DIR="$(mktemp -d)"
 
-git clone --depth 1 --single-branch --branch ${BRANCH} "$ROOT/git_cache/Wikibase.git" "$TEMP_GIT_DIR"
-git -C "$TEMP_GIT_DIR" submodule update
+git clone --depth 1 --single-branch --branch ${BRANCH} "$ROOT/git_cache/Wikibase.git" "$TEMP_GIT_DIR/Wikibase"
+git -C "$TEMP_GIT_DIR/Wikibase" submodule update
 
 # remove git things from release package
-rm "$TEMP_GIT_DIR/".git* -rfv
+rm "$TEMP_GIT_DIR/Wikibase/".git* -rfv
 
-GZIP=-9 tar -zcvf "$TEMP_TAR_DIR"/Wikibase.tar.gz "$TEMP_GIT_DIR"
+GZIP=-9 tar -C "$TEMP_GIT_DIR" -zcvf "$TEMP_TAR_DIR"/Wikibase.tar.gz Wikibase
 
 echo "TARBALL_PATH="$TEMP_TAR_DIR/Wikibase.tar.gz"" >> $GITHUB_ENV
