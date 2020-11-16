e0c530bd618334e6f42843d345c9eea68d80c81f
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 15:03:31 +0000

    try to debug docker test env; also add partial test for curling wdqs

--- test_scripts/run-tests.sh
+++ test_scripts/run-tests.sh
@@ -8,14 +8,39 @@ else
     exit 1
 fi
 
-
-# QueryService test
-if curl --fail --show-error --output /dev/null --silent $QUERYSERVICE_SERVER/; then
+# QueryService test blazegraph root page
+if curl --fail --show-error --output /dev/null --silent $QUERYSERVICE_SERVER/bigdata/namespace/wdq/sparql; then
     echo 'Successfully loaded the root page!'
 else
     echo "Could not retrieve root page"
     exit 1
 fi
+
+# QueryService test simple SPARQL query
+if curl --fail --show-error --output /dev/null --silent $QUERYSERVICE_SERVER/bigdata/namespace/wdq/sparql; then
+    echo 'Successfully loaded the root page!'
+else
+    echo "Could not retrieve root page"
+    exit 1
+fi
+
+
+echo curl '$QUERYSERVICE_SERVER/bigdata/namespace/wdq/sparql' \ 
+    -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0' \ 
+    -H 'Accept: application/sparql-results+json' \ 
+    -H 'Accept-Language: en-US,en;q=0.5' \ 
+    --compressed \ 
+    -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \ 
+    -H 'X-Requested-With: XMLHttpRequest' \ 
+    -H 'Origin: http://localhost:8989' \ 
+    -H 'Connection: keep-alive' \
+    -H 'Referer: http://localhost:8989/bigdata/' \
+    --data-raw 'query=+SELECT+*+WHERE%7B+%3Fs+%3Fp+%3Fo+%7D' \
+    --fail \ 
+    --show-error
+    #--output /dev/null \
+    #--silent
+
 
 # QueryService UI test
 if curl --fail --show-error --output /dev/null --silent $QUERYSERVICE_UI_SERVER/; then
@@ -24,3 +49,5 @@ else
     echo "Could not retrieve UI root page"
     exit 1
 fi
+
+
--- test_scripts/test_docker.sh
+++ test_scripts/test_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd test_scripts
+ls
+ls ../
 docker load -i "../$WIKIBASE_IMAGE_NAME.docker.tar.gz"
+docker load -i "../$QUERYSERVICE_IMAGE_NAME.docker.tar.gz"
+docker load -i "../$QUERYSERVICE_UI_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
 docker-compose -f docker-compose.yml -f docker-compose-test.yml build wikibase-test
 docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh
