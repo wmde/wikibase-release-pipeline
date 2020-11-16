417426d6a13040f86e00f6b2f2030d83cb37911d
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 14:08:04 +0000

    fix git cache pull folder 2

--- update_cache.sh
+++ update_cache.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 extensions=("Wikibase")
 
 for extension in "${extensions[@]}"; do
-	if [ ! -d "git_cache/mediawiki/extensions/$extension.git" ]; then
+	if [ ! -d "git_cache/$extension.git" ]; then
 		git clone --bare "https://gerrit.wikimedia.org/r/mediawiki/extensions/$extension.git" "git_cache/$extension.git"
 	fi
 	cd "git_cache/$extension.git"
