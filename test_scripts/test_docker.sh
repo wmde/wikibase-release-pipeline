680ce84a484ed1d1a21fe42c82230936f21c166f
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 14:36:58 +0000

    more explictly build test image

--- test_scripts/test_docker.sh
+++ test_scripts/test_docker.sh
@@ -3,4 +3,5 @@
 cd test_scripts
 docker load -i "../$WIKIBASE_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
-docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh --build
+docker-compose -f docker-compose.yml -f docker-compose-test.yml build wikibase-test
+docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh
