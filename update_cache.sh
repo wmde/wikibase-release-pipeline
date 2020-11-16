91e7be91656060d7aa9f0e64d6bf2673090ee45f
Thomas Arrow <thomasarrow@gmail.com>
2020-11-16 14:07:01 +0000

    fix git cache pull folder

--- update_cache.sh
+++ update_cache.sh
@@ -8,7 +8,7 @@ for extension in "${extensions[@]}"; do
 	if [ ! -d "git_cache/mediawiki/extensions/$extension.git" ]; then
 		git clone --bare "https://gerrit.wikimedia.org/r/mediawiki/extensions/$extension.git" "git_cache/$extension.git"
 	fi
-	cd "git_cache/mediawiki/extensions/$extension.git"
+	cd "git_cache/$extension.git"
 	git fetch --all
 	cd -
 done
