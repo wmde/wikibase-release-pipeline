818c6fcc8fe914412bc3226dfc0bb0dcf26ed021
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-11-17 19:22:43 +0100

    add some comments, enable curl tests

--- test_scripts/test_docker.sh
+++ test_scripts/test_docker.sh
@@ -6,13 +6,17 @@ docker load -i "../$WIKIBASE_IMAGE_NAME.docker.tar.gz"
 docker load -i "../$QUERYSERVICE_IMAGE_NAME.docker.tar.gz"
 docker load -i "../$QUERYSERVICE_UI_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
-#docker-compose -f docker-compose.yml -f docker-compose-test.yml build wikibase-test
-#docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh
 
+# build curl test container 
+docker-compose -f docker-compose.yml -f docker-compose-test.yml build wikibase-test
+# run curl tests
+docker-compose -f docker-compose.yml -f docker-compose-test.yml run wikibase-test bash /run-tests.sh
+
+## build selenium test container
 docker-compose -f docker-compose.yml \
     -f docker-compose-selenium-test.yml \
     build wikibase-selenium-test
-
+## run selenium tests
 docker-compose -f docker-compose.yml \
     -f docker-compose-selenium-test.yml \
     run wikibase-selenium-test
