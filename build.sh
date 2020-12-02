e23daa24f41ac3f10a31fb086a3a3565acc8445a
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-12-02 10:26:01 +0100

    remove tty

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build . -t builder && \
- docker run -it \
+ docker run -i \
  -v $(pwd)/artifacts:/artifacts \
  -v $(pwd)/git_cache:/app/git_cache \
  -v /var/run/docker.sock:/var/run/docker.sock \
