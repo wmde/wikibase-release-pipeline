a1f02584e11b32a9de9e60867ae3b452f3981845
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-12-02 11:57:31 +0100

    update path

--- test_scripts/test_docker.sh
+++ test_scripts/test_docker.sh
@@ -8,9 +8,9 @@ rm -f log/*
 # does not exists on github
 mkdir log -p
 
-docker load -i "../$WIKIBASE_IMAGE_NAME.docker.tar.gz"
-docker load -i "../$QUERYSERVICE_IMAGE_NAME.docker.tar.gz"
-docker load -i "../$QUERYSERVICE_UI_IMAGE_NAME.docker.tar.gz"
+docker load -i "../artifacts/$WIKIBASE_IMAGE_NAME.docker.tar.gz"
+docker load -i "../artifacts/$QUERYSERVICE_IMAGE_NAME.docker.tar.gz"
+docker load -i "../artifacts/$QUERYSERVICE_UI_IMAGE_NAME.docker.tar.gz"
 docker-compose up -d
 
 docker-compose logs -f --no-color > log/wikibase.log &
