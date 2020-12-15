d9e2c94188ac3bc83be42947eddec59fe7d9e4db
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-12-15 13:07:19 +0100

    add versions env

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM docker:latest
-
 RUN apk add --no-cache git make bash
 
 WORKDIR "/app/"
 ADD Docker Docker
 ADD build_scripts/ build_scripts
 ADD Makefile Makefile
-ADD .env .env
+
+ARG RELEASE_ENV_FILE
+ADD ${RELEASE_ENV_FILE} .env
+
 ADD update_cache.sh update_cache.sh
--- build_github.sh
+++ build_github.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-docker build . -t builder && \
+TARGET=$1
+RELEASE_ENV_FILE=$2
+
+if [ ! -f "$RELEASE_ENV_FILE" ]; then
+    echo "RELEASE_ENV_FILE is not set. Defaulting to .env"
+    RELEASE_ENV_FILE=.env
+fi
+
+echo
+cat $RELEASE_ENV_FILE
+echo
+
+docker build --build-arg RELEASE_ENV_FILE=$RELEASE_ENV_FILE . -t builder && \
  docker run -i \
  -v $(pwd)/artifacts:/app/artifacts \
  -v $(pwd)/git_cache:/app/git_cache \
  -v /var/run/docker.sock:/var/run/docker.sock \
- builder:latest make $1
+ builder:latest make $TARGET
