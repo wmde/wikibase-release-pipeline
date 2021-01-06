8d2209e4b0b5570f90dfe2a53e4843575d30724c
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2021-01-06 14:30:52 +0000

    include bearer in header

--- publish_scripts/download_artifacts/download_artifacts.py
+++ publish_scripts/download_artifacts/download_artifacts.py
@@ -14,7 +14,7 @@ print( 'token:' + os.getenv('GITHUB_TOKEN')[1] + 'token ends' )
 for artifact in artifactsMetadata['artifacts']:
     # if artifact['name'] != 'QueryService UI Docker Image':
     #     continue
-    r = requests.get( artifact['archive_download_url'], allow_redirects=True, headers={"Authorization": os.getenv('GITHUB_TOKEN') } )
+    r = requests.get( artifact['archive_download_url'], allow_redirects=True, headers={"Authorization": 'Bearer ' + os.getenv('GITHUB_TOKEN') } )
     zipfilePath = os.path.join('/zips' ,artifact['name'] + '.zip')
     open(zipfilePath, 'wb').write(r.content)
     with zipfile.ZipFile(zipfilePath, 'r') as zip_ref:
