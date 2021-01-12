c725e926b17c73acf6b81125efc4099f35082703
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-12 12:11:42 +0100

    add some more logging

--- Docker/tag_git/tag_git.sh
+++ Docker/tag_git/tag_git.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# contains the commit hashes
 BUILD_METADATA_ENV_FILE=/extractedArtifacts/BuildMetadata/build_metadata.env
 
 if [ ! -f $BUILD_METADATA_FILE ]; then
+    echo "This build does not contain a metadata file!"
     exit 1
 fi
 
@@ -18,4 +20,9 @@ fi
 
 git clone --single-branch --branch ${WIKIBASE_BRANCH_NAME} "/git_cache/Wikibase.git" "/repo/Wikibase"
 cd /repo/Wikibase
-git tag -a $RELEASE_VERSION $WIKIBASE_COMMIT_HASH -m $WORKFLOW_RUN_NUMBER
+
+echo "Tagging $RELEASE_VERSION at $WIKIBASE_COMMIT_HASH"
+git tag -a $RELEASE_VERSION $WIKIBASE_COMMIT_HASH -m "Tagging: $RELEASE_VERSION Build: $WORKFLOW_RUN_NUMBER"
+
+# set remote
+# push origin
--- publish_git_tag.sh
+++ publish_git_tag.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd publish_scripts
-#docker-compose build download_artifacts && docker-compose run download_artifacts
-docker-compose build tag_git && docker-compose run tag_git
+docker-compose build download_artifacts && docker-compose run download_artifacts
+docker-compose build tag_git && docker-compose run tag_git --rm
