644cf43b0e63e724fe3a65ad6197658bd882309b
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-12 11:08:54 +0100

    check for the variables

--- Docker/tag_git/tag_git.sh
+++ Docker/tag_git/tag_git.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-ls /extractedArtifacts/BuildMetadata/
+BUILD_METADATA_ENV_FILE=/extractedArtifacts/BuildMetadata/build_metadata.env
+
+if [ ! -f $BUILD_METADATA_FILE ]; then
+    exit 1
+fi
+
+source $BUILD_METADATA_ENV_FILE
+
+if [ -z $WIKIBASE_BRANCH_NAME ] || \
+[ -z $RELEASE_VERSION ] || \
+[ -z $WIKIBASE_COMMIT_HASH ] || \
+[ -z $WORKFLOW_RUN_NUMBER ] ; then
+    echo "A variable is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+    exit 1;
+fi
+
 git clone --single-branch --branch ${WIKIBASE_BRANCH_NAME} "/git_cache/Wikibase.git" "/repo/Wikibase"
 cd /repo/Wikibase
-git tag -a $RELEASE_VERSION -m $WORKFLOW_RUN_NUMBER
+git tag -a $RELEASE_VERSION $WIKIBASE_COMMIT_HASH -m $WORKFLOW_RUN_NUMBER
