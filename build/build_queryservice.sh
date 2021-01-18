32cc5ce1852ec46b9c17e9c0e93ee4d3f8e0fbe4
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-18 18:39:27 +0100

    change variable name

--- build/build_queryservice.sh
+++ build/build_queryservice.sh
@@ -5,15 +5,15 @@ TARBALL="service-$QUERYSERVICE_VERSION-dist.tar.gz"
 TARBALL_SIGNATURE="$TARBALL".md5
 
 MD5_SIGNATURE="$(wget -qO- https://archiva.wikimedia.org/repository/releases/org/wikidata/query/rdf/service/$QUERYSERVICE_VERSION/$TARBALL_SIGNATURE)"
-MD5_PATH="git_cache/queryservice/$MD5_SIGNATURE"
+CACHE_PATH="git_cache/queryservice/$MD5_SIGNATURE"
 
-if [ ! -f "$MD5_PATH/$TARBALL" ]; then
-    mkdir -p "$MD5_PATH"
+if [ ! -f "$CACHE_PATH/$TARBALL" ]; then
+    mkdir -p "$CACHE_PATH"
     wget "https://archiva.wikimedia.org/repository/releases/org/wikidata/query/rdf/service/$QUERYSERVICE_VERSION/$TARBALL" \
-        -O "$MD5_PATH/$TARBALL"
+        -O "$CACHE_PATH/$TARBALL"
 fi
 
-TARBALL_PATH="$MD5_PATH/$TARBALL"
+TARBALL_PATH="$CACHE_PATH/$TARBALL"
 
 if [ -n "$GITHUB_ENV" ]; then
     echo "TARBALL_PATH=$TARBALL_PATH" >> "$GITHUB_ENV"
