0b63feb865daef4df5ee6858a43e160fcc87f293
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-19 18:00:16 +0100

    move to cache, add some comments

--- build/build_queryservice.sh
+++ build/build_queryservice.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# Build script for queryservice
+# Downloads the service-dist.tar file from archiva and caches it 
 set -e
 
 TARBALL="service-$QUERYSERVICE_VERSION-dist.tar.gz"
 TARBALL_SIGNATURE="$TARBALL".md5
 
-MD5_SIGNATURE="$(wget -qO- https://archiva.wikimedia.org/repository/releases/org/wikidata/query/rdf/service/$QUERYSERVICE_VERSION/$TARBALL_SIGNATURE)"
-CACHE_PATH="git_cache/queryservice/$MD5_SIGNATURE"
+MD5_SIGNATURE="$(wget -qO- "https://archiva.wikimedia.org/repository/releases/org/wikidata/query/rdf/service/$QUERYSERVICE_VERSION/$TARBALL_SIGNATURE")"
+CACHE_PATH="cache/queryservice/$MD5_SIGNATURE"
 
 if [ ! -f "$CACHE_PATH/$TARBALL" ]; then
     mkdir -p "$CACHE_PATH"
