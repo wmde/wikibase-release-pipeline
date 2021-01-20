9379803cd626a90b6497787bc6e0483f25037d88
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-20 14:43:27 +0100

    reorganize folders

--- test/run_selenium.sh
+++ test/run_selenium.sh
@@ -2,8 +2,22 @@
 # shellcheck disable=SC2086
 set -ex
 
+export SUITE=$1
 
-SUITE_CONFIG="-f docker-compose.yml -f docker-compose.override.yml"
+if [ -z "$SUITE" ]; then
+    echo "SUITE is not set"
+    exit 1
+fi
+
+DEFAULT_SUITE_CONFIG="-f docker-compose.yml"
+SUITE_OVERRIDE="suite-config/$SUITE/docker-compose.override.yml"
+SUITE_CONFIG="$DEFAULT_SUITE_CONFIG"
+
+
+if [ -f "$SUITE_OVERRIDE" ]; then
+    echo "Using docker-compose override file $SUITE_OVERRIDE"
+    SUITE_CONFIG="$DEFAULT_SUITE_CONFIG -f $SUITE_OVERRIDE"
+fi
 
 # start container with settings
 docker-compose $SUITE_CONFIG up -d --force-recreate
@@ -13,11 +27,10 @@ docker-compose logs -f --no-color > "log/wikibase.$1.log" &
 docker-compose $SUITE_CONFIG -f docker-compose-curl-test.yml build wikibase-test
 docker-compose $SUITE_CONFIG -f docker-compose-curl-test.yml run wikibase-test
 
-export SUITE=$1
 
 docker-compose \
     $SUITE_CONFIG -f docker-compose-selenium-test.yml \
     run \
     wikibase-selenium-test npm run "test:$1"
 
-#docker-compose down
+docker-compose $SUITE_CONFIG -f docker-compose-selenium-test.yml down
