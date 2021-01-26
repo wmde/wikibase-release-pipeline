d0ac88fd8463de6698a36242c731c9216763cf09
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-26 08:42:47 +0100

    fix review comment

--- Docker/test/selenium/specs/repo/special-version.js
+++ Docker/test/selenium/specs/repo/special-version.js
@@ -11,15 +11,18 @@ describe( 'Special:Version', function () {
 
 	// should be disabled for dynamic tests
 	// eslint-disable-next-line mocha/no-setup-in-describe
-	[ 'wikibase', 'other' ].forEach( ( extensionPackage ) => {
+	Object.keys( extensions ).forEach( ( extensionPackage ) => {
+
 		it( 'Should contain ' + extensionPackage + ' extensions', function () {
 			browser.url( process.env.MW_SERVER + '/wiki/Special:Version' );
 
 			extensions[ extensionPackage ].forEach( ( extension ) => {
+
 				// /wiki/Special:Version generate these for each installed extension
 				const elementSelector = $( '#mw-version-ext-' + extensionPackage + '-' + extension );
 				elementSelector.waitForDisplayed();
 				elementSelector.scrollIntoView();
+
 				assert( elementSelector.getText() !== null );
 			} );
 		} );
