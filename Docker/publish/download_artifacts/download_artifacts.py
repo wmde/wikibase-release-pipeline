b1e45a8fc806d88a8d74db5121b041f0d48e3aca
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-01-29 17:48:10 +0100

    restore changing flags

--- Docker/publish/download_artifacts/download_artifacts.py
+++ Docker/publish/download_artifacts/download_artifacts.py
@@ -26,5 +26,6 @@ if not os.path.exists(basepath):
             extractedFileFath = os.path.join(basepath, artifact['name'])
             zip_ref.extractall(extractedFileFath)
 
+os.system('chmod -R 0777 ' + basepath)
 
 print(os.system('ls -lh ' + basepath))
