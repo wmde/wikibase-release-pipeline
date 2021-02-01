f81e72429251791e68c248f858bdbcadb202cece
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-02-01 10:20:34 +0100

    use retry for all curl-tests

--- Docker/test/curl/test_curl.sh
+++ Docker/test/curl/test_curl.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-# Wikibase test
-if curl --fail --retry 60 --retry-all-errors --retry-delay 1 --max-time 10 --retry-max-time 60 --show-error --output /dev/null --silent "$WIKIBASE_SERVER"/wiki/Main_Page; then
-    echo 'Successfully loaded the wiki main page!'
-else
-    echo "Could not retrieve main page"
-    exit 1
-fi
+function check_if_up {
+    URL=$1
+    if curl --fail --retry 60 --retry-all-errors --retry-delay 1 --max-time 10 --retry-max-time 60 --show-error --output /dev/null --silent $URL; then
+        echo "Successfully loaded $URL!"
+    else
+        echo "Could not retrieve $URL"
+        exit 1
+    fi
+}
 
-# QueryService test blazegraph root page
-if curl --fail --show-error --output /dev/null --silent "$QUERYSERVICE_SERVER"/bigdata/namespace/wdq/sparql; then
-    echo 'Successfully loaded the QueryService page!'
-else
-    echo "Could not retrieve root page"
-    exit 1
-fi
+check_if_up "$WIKIBASE_SERVER"/wiki/Main_Page
 
-# QueryService UI test
-if curl --fail --show-error --output /dev/null --silent "$QUERYSERVICE_UI_SERVER"/; then
-    echo 'Successfully loaded the QueryService UI page!'
-else
-    echo "Could not retrieve UI root page"
-    exit 1
-fi
+check_if_up "$QUERYSERVICE_SERVER"/bigdata/namespace/wdq/sparql
 
-# QueryService test simple SPARQL query
-SPARQL_QUERY='SELECT * WHERE{ ?s ?p ?o }'
-echo "Executing SPARQL query $SPARQL_QUERY ..."
-curl "$QUERYSERVICE_SERVER/bigdata/namespace/wdq/sparql" \
-    --compressed \
-    --data-urlencode "query=$SPARQL_QUERY" \
-    --silent \
-    --fail \
-    --show-error \
-    --output /dev/null || exit 1
+check_if_up "$QUERYSERVICE_UI_SERVER"
+
+check_if_up "$QUERYSERVICE_SERVER/bigdata/namespace/wdq/sparql"
