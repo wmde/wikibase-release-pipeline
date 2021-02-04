512757a993caf8097d5ee1ea5daa5225bdca256c
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-02-04 14:22:26 +0100

    some fixes for building REL1_35 branch
    - mw REL1_35 for some reasons contain the Vector folder so this is now
    removed always and cloned.
    - Fedprops auto-setup is not in REL1_35 so we can use the manual setup
    to skip relying on it.

--- test/suite-config/fedprops/LocalSettings.php
+++ test/suite-config/fedprops/LocalSettings.php
@@ -8,3 +8,23 @@ $wgDebugDumpSql  = true;
 $wgShowDBErrorBacktrace = true;
 
 $wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+$wgWBRepoSettings['federatedPropertiesSourceScriptUrl'] = 'https://www.wikidata.org/w/';
+
+$wgWBRepoSettings['entitySources'] = [
+    'local' => [
+        'entityNamespaces' => [ 'item' => 120 ],
+        'repoDatabase' => false,
+        'baseUri' => 'http://wikibase.svc/entity/',
+        'interwikiPrefix' => '',
+        'rdfNodeNamespacePrefix' => 'wd',
+        'rdfPredicateNamespacePrefix' => 'wdt',
+    ],
+    'fedprops' => [
+        'entityNamespaces' => [ 'property' => 122 ],
+        'repoDatabase' => false,
+        'baseUri' => 'http://www.wikidata.org/entity/',
+        'interwikiPrefix' => 'wikidata',
+        'rdfNodeNamespacePrefix' => 'fpwd',
+        'rdfPredicateNamespacePrefix' => 'fpwd',
+    ],
+];
