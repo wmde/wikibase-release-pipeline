303f00aa7ef28e1aee7579779b738c56873b3d72
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-02-04 18:55:08 +0100

    load image

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -17,6 +17,7 @@ docker --version
 docker load -i "../artifacts/$WIKIBASE_IMAGE_NAME.docker.tar.gz"
 docker load -i "../artifacts/$QUERYSERVICE_IMAGE_NAME.docker.tar.gz"
 docker load -i "../artifacts/$QUERYSERVICE_UI_IMAGE_NAME.docker.tar.gz"
+docker load -i "../artifacts/$ELASTICSEARCH_IMAGE_NAME.docker.tar.gz"
 
 export LOCALSETTINGS_VARIANT=$1
 
