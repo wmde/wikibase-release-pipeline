360143b7fcac12e23b674c6fb60f7e8e0ba5485d
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-02-09 16:54:31 +0100

    prefix metadata variables with METADATA_

--- build/build_queryservice_ui.sh
+++ build/build_queryservice_ui.sh
@@ -5,7 +5,13 @@ ROOT="$(pwd)"
 WDQS_UI_GIT_DIR="$(mktemp -d)"
 TARBALL_PATH="$ROOT"/artifacts/wdqs-frontend.tar.gz
 
-UPDATE_SUBMODULE=1 bash "$ROOT"/build/clone_repo.sh "$QUERYSERVICE_UI_COMMIT_HASH" "$ROOT/git_cache/services/wikidata-query-gui.git" wdqs_ui "$WDQS_UI_GIT_DIR" master
+UPDATE_SUBMODULE=1 bash "$ROOT"/build/clone_repo.sh \
+    "$QUERYSERVICE_UI_COMMIT_HASH" \
+    "$ROOT/git_cache/services/wikidata-query-gui.git" \
+    WDQS_UI \
+    "$WDQS_UI_GIT_DIR" \
+    master
+
 bash "$ROOT"/build/clean_repo.sh "$WDQS_UI_GIT_DIR"
 
 GZIP=-9 tar -C "$WDQS_UI_GIT_DIR" -zcvf "$TARBALL_PATH" .
