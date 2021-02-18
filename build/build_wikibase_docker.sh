7f89c518a26184e7359aaa62bbb944d522b0c8bf
toan <tobias.andersson@wikimedia.de>
2021-02-18 17:54:10 +0100

    dont use package version, add parameters to wikibase build

--- build/build_wikibase_docker.sh
+++ build/build_wikibase_docker.sh
@@ -12,6 +12,10 @@ fi
 
 cp Docker/build/wait-for-it.sh Docker/build/Wikibase/artifacts/
 
-docker build --build-arg MEDIAWIKI_IMAGE_NAME="$MEDIAWIKI_IMAGE_NAME" Docker/build/Wikibase/ -t "$1"
+docker build \
+    --build-arg MEDIAWIKI_IMAGE_NAME="$MEDIAWIKI_IMAGE_NAME" \
+    --build-arg COMPOSER_IMAGE_NAME="$COMPOSER_IMAGE_NAME" \
+    --build-arg COMPOSER_IMAGE_VERSION="$COMPOSER_IMAGE_VERSION" \
+    Docker/build/Wikibase/ -t "$1"
 
 docker save "$1" | gzip -"$GZIP_COMPRESSION_RATE"f > artifacts/"$1".docker.tar.gz
