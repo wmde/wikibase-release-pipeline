28cb2d6fc8ba544ce7850509878ecdb09d25a4e3
toan <tobias.andersson@wikimedia.de>
2021-03-02 12:46:05 +0100

    Test pingback

---
+++ Docker/test/selenium/specs/pingback/pingback.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const assert = require( 'assert' );
+const defaultFunctions = require( '../../helpers/default-functions' );
+
+describe( 'Pingback', function () {
+
+	before( function () {
+		defaultFunctions();
+	} );
+
+	it( 'Should ping on first page request', function () {
+
+		browser.url( process.env.MW_SERVER + '/wiki/Main_Page' );
+
+		browser.pause( 5 * 1000 );
+
+		const sqlResult = browser.dockerExecute(
+			process.env.DOCKER_MYSQL_NAME,
+			'mysql --user ' + process.env.DB_USER +
+			' --password=' + process.env.DB_PASS + ' ' + process.env.DB_NAME +
+			" -e 'SELECT * from updatelog where ul_key LIKE \"WikibasePingback%\"'"
+		);
+
+		assert( sqlResult.includes( 'WikibasePingback' ) === true );
+		assert( sqlResult.includes( 'WikibasePingback-1.' ) === true );
+
+		const result = browser.makeRequest( process.env.PINGBACK_BEACON_SERVER );
+		assert( result.data.length === 2 );
+
+		const requestData = JSON.parse(
+			Object.keys( result.data[ 0 ] )[ 0 ].replace( ';', '' )
+		);
+
+		assert( requestData.schema === 'WikibasePingback' );
+	} );
+
+} );
