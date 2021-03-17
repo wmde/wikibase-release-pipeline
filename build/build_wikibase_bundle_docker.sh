cbae002cf5eefe86d4d68c51100e225bcb12cf8d
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-03-17 21:25:25 +0100

    [T195725] Add Nuke Extension + test (#106)
    Co-authored-by: Thomas Arrow <thomas.arrow_ext@wikimedia.de>

--- build/build_wikibase_bundle_docker.sh
+++ build/build_wikibase_bundle_docker.sh
@@ -19,6 +19,7 @@ bash build/build_extension.sh Elastica "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT
 bash build/build_extension.sh CirrusSearch "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT_EXTENSIONS_PATH"
 bash build/build_extension.sh VisualEditor "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT_EXTENSIONS_PATH"
 bash build/build_extension.sh WikibaseCirrusSearch "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT_EXTENSIONS_PATH"
+bash build/build_extension.sh Nuke "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT_EXTENSIONS_PATH"
 bash build/build_extension.sh WikibaseManifest "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT_EXTENSIONS_PATH"
 bash build/build_extension.sh cldr "${GERRIT_EXTENSION_BRANCH_NAME}" "$BUILT_EXTENSIONS_PATH"
 
