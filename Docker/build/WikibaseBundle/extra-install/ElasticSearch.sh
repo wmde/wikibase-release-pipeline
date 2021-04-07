d7418e99c3ac19705ab50392592c2681d29ff0ac
toan <tobias.andersson@wikimedia.de>
2021-04-07 19:36:22 +0200

    Create ElasticSearch index on startup

--- Docker/build/WikibaseBundle/extra-install/ElasticSearch.sh
+++ Docker/build/WikibaseBundle/extra-install/ElasticSearch.sh
@@ -3,3 +3,7 @@ set -ex
 
 export DOLLAR='$'
 envsubst < /var/www/html/LocalSettings.template.d/LocalSettings.Elasticsearch.php.template > /var/www/html/LocalSettings.d/Elasticsearch.php
+
+php /var/www/html/extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php
+php /var/www/html/extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipParse
+php /var/www/html/extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks --indexOnSkip
