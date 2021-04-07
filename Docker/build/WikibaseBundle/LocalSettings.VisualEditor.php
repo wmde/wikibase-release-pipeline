f6f6cde41f25b3649c9dffe453b205fd1038cc26
toan <tobias.andersson@wikimedia.de>
2021-04-07 17:20:44 +0200

    Fixes visual-editor configuration

--- Docker/build/WikibaseBundle/LocalSettings.VisualEditor.php
+++ Docker/build/WikibaseBundle/LocalSettings.VisualEditor.php
@@ -4,3 +4,7 @@
 ## VisualEditor Extension
 wfLoadExtension( 'VisualEditor' );
 wfLoadExtension( 'Parsoid', 'vendor/wikimedia/parsoid/extension.json' );
+
+$wgVirtualRestConfig['modules']['parsoid'] = array(
+    'url' => 'http://' . $_ENV['WIKIBASE_HOST'] . $wgScriptPath . '/rest.php',
+);
