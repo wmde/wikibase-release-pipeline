d053369d3c3988d10aee242a8c892473084e1806
toan <tobias.andersson@wikimedia.de>
2021-04-12 20:00:35 +0200

    Test base image

--- test/run_selenium.sh
+++ test/run_selenium.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 # shellcheck disable=SC2086
-set -ex
+set -e
 
 export SUITE=$1
 
-if [ -z "$SUITE" ]; then
-    echo "SUITE is not set"
+# if prepended with base__ we might still want to use the bundle config
+if [[ $SUITE == base__* ]] && [ ! -d "suite-config/$SUITE" ] ; then
+    SUITE_CONFIG_NAME=${SUITE//base__/}
+else
+    SUITE_CONFIG_NAME=$SUITE
+fi
+
+# set suite localsettings
+export LOCALSETTINGS_VARIANT=$SUITE_CONFIG_NAME
+
+if [ ! -d "suite-config/$SUITE_CONFIG_NAME" ]; then
+    echo "Suite $SUITE_CONFIG_NAME does not exist"
     exit 1
 fi
 
-if [ -z "$FILTER" ]; then
-    export FILTER="*"
-fi
-
-if [ -z "$DATABASE_IMAGE_NAME" ]; then
-    export DATABASE_IMAGE_NAME="$DEFAULT_DATABASE_IMAGE_NAME"
-fi
-
-# remove reporter log
-rm -f "log/selenium/result-$SUITE.json"
-
 DEFAULT_SUITE_CONFIG="-f docker-compose.yml"
-SUITE_OVERRIDE="suite-config/$SUITE/docker-compose.override.yml"
+SUITE_OVERRIDE="suite-config/$SUITE_CONFIG_NAME/docker-compose.override.yml"
 SUITE_CONFIG="$DEFAULT_SUITE_CONFIG"
 
 ALL_SUITES_FILES="$(find suite-config/ -name "docker-compose.override.yml")"
@@ -30,11 +29,13 @@ for FILE in $ALL_SUITES_FILES; do
     ALL_SUITES="$ALL_SUITES -f $FILE"
 done
 
-
 if [ -f "$SUITE_OVERRIDE" ]; then
     echo "Using docker-compose override file $SUITE_OVERRIDE"
     SUITE_CONFIG="$DEFAULT_SUITE_CONFIG -f $SUITE_OVERRIDE"
 fi
+
+# remove reporter log
+rm -f "log/selenium/result-$SUITE.json"
 
 # stop any dangling things
 docker-compose $DEFAULT_SUITE_CONFIG $ALL_SUITES -f docker-compose-selenium-test.yml down --volumes
@@ -48,7 +49,12 @@ docker-compose $SUITE_CONFIG logs -f --no-color > "log/wikibase.$STRING_DATABASE
 docker-compose $SUITE_CONFIG -f docker-compose-curl-test.yml build wikibase-test
 docker-compose $SUITE_CONFIG -f docker-compose-curl-test.yml run wikibase-test
 
+NODE_COMMAND='test:run'
+if [ -n "$FILTER" ]; then
+    NODE_COMMAND='test:run-filter'
+fi
+
 docker-compose \
     $SUITE_CONFIG -f docker-compose-selenium-test.yml \
     run \
-    wikibase-selenium-test npm run "test:run"
+    wikibase-selenium-test npm run $NODE_COMMAND
