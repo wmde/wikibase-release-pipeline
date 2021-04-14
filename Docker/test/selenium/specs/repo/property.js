d87d7bd1ee48960be95b8b0cf3ca03991b00965a
Tobias Andersson <tobias.andersson@wikimedia.de>
2021-04-14 09:09:47 +0200

    Remove hardcoded property ids from repo/property.js (#126)

--- Docker/test/selenium/specs/repo/property.js
+++ Docker/test/selenium/specs/repo/property.js
@@ -17,15 +17,19 @@ describe( 'Property', function () {
 
 		propertyId = browser.call( () => WikibaseApi.getProperty( 'string' ) );
 
+		const propertyIdSelector = '=' + propertyId + ' (' + propertyId + ')'; // =P1 (P1)
+
 		Property.open( propertyId );
 		Property.addStatement.waitForDisplayed();
 		Property.addStatement.click();
 
-		browser.keys( [ 'P', '1' ] );
-		$( '=P1 (P1)' ).waitForDisplayed();
-		$( '=P1 (P1)' ).click();
+		// fill out property id for statement
+		browser.keys( propertyId.split( '' ) );
+		$( propertyIdSelector ).waitForDisplayed();
+		$( propertyIdSelector ).click();
 		browser.keys( [ 'S', 'T', 'A', 'T', 'E', 'M', 'E', 'N', 'T' ] );
 
+		// wait for save button to re-enable
 		browser.pause( 1000 * 1 );
 		Property.save.click();
 		browser.pause( 1000 * 2 );
@@ -33,23 +37,23 @@ describe( 'Property', function () {
 		Property.addReference.waitForDisplayed();
 		Property.addReference.click();
 
+		// fill out property id for reference
 		$( '.ui-entityselector-input' ).waitForDisplayed();
-
-		browser.keys( [ 'P', '1' ] );
-		$( '=P1 (P1)' ).waitForDisplayed();
-		$( '=P1 (P1)' ).click();
+		browser.keys( propertyId.split( '' ) );
+		$( propertyIdSelector ).waitForDisplayed();
+		$( propertyIdSelector ).click();
 		browser.keys( [ 'R', 'E', 'F', 'E', 'R', 'E', 'N', 'C', 'E' ] );
 
 		browser.pause( 1000 * 1 );
 		Property.save.click();
 
-		Property.open( 'P1' );
+		Property.open( propertyId );
 	} );
 
 	it( 'Should contain statement and reference in EntityData', function () {
 		const response = browser.makeRequest( process.env.MW_SERVER + '/wiki/Special:EntityData/' + propertyId + '.json' );
 		const body = response.data;
-		const claim = body.entities.P1.claims[ propertyId ][ 0 ];
+		const claim = body.entities[ propertyId ].claims[ propertyId ][ 0 ];
 		const reference = claim.references[ 0 ].snaks[ propertyId ][ 0 ];
 
 		assert( claim.mainsnak.datavalue.value === 'STATEMENT' );
