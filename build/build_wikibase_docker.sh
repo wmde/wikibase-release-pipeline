0ecb23f84a94b4ab9a4ba096b4457c5d48394090
Tobias <tobias.andersson@wikimedia.de>
2021-07-16 15:33:00 +0200

    Stop building the custom MW image, use the official ones (#187)
    Bug: T285680

--- build/build_wikibase_docker.sh
+++ build/build_wikibase_docker.sh
@@ -4,7 +4,7 @@ set -ex
 mkdir -p Docker/build/Wikibase/artifacts
 mkdir -p Docker/build/Wikibase/artifacts/extensions
 
-docker load -i "artifacts/mediawiki.docker.tar.gz"
+MEDIAWIKI_IMAGE_VERSION="$MEDIAWIKI_VERSION"
 
 if [ -f "$TARBALL_PATH" ]; then
     cp "$TARBALL_PATH" Docker/build/Wikibase/artifacts/
@@ -14,6 +14,7 @@ cp Docker/build/wait-for-it.sh Docker/build/Wikibase/artifacts/
 
 docker build \
     --build-arg MEDIAWIKI_IMAGE_NAME="$MEDIAWIKI_IMAGE_NAME" \
+    --build-arg MEDIAWIKI_IMAGE_VERSION="$MEDIAWIKI_IMAGE_VERSION" \
     --build-arg COMPOSER_IMAGE_NAME="$COMPOSER_IMAGE_NAME" \
     --build-arg COMPOSER_IMAGE_VERSION="$COMPOSER_IMAGE_VERSION" \
     Docker/build/Wikibase/ -t "$1"
