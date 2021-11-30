c701ff028a050bd4f4706f1f1962cefb403a18fd
toan <tobias.andersson@wikimedia.de>
2021-11-30 10:35:52 +0100

    Stop using custom cloning / cleaning for
    - wikibase
    - extensions

--- build/build_extension.sh
+++ build/build_extension.sh
@@ -10,19 +10,18 @@ FINAL_EXTENSION_PATH=$3
 TEMP_GIT_DIR="$(mktemp -d)"
 TEMP_TAR_DIR="$(pwd)"/artifacts
 EXT_PATH="$TEMP_GIT_DIR/$EXTENSION"
-
-EXTENSION_COMMIT_HASH_FILE="$ROOT/artifacts/build_metadata_${EXTENSION,,}.env"
 EXTENSION_COMMIT_HASH_VARIABLE="${EXTENSION^^}_COMMIT_HASH"
 
 mkdir -p "$TEMP_TAR_DIR"
 
-git clone --depth 1 --single-branch --branch "${BRANCH_NAME}" "$ROOT/git_cache/$EXTENSION.git" "$EXT_PATH"
+COMMIT_HASH=${!EXTENSION_COMMIT_HASH_VARIABLE}
+UPDATE_SUBMODULE=1 bash "$ROOT"/build/clone_repo.sh \
+    "$COMMIT_HASH" \
+    "$ROOT/git_cache/$EXTENSION.git" \
+    "$EXTENSION" \
+    "$EXT_PATH" \
+    "${BRANCH_NAME}"
 
-GIT_TRACE=1 git -C "$EXT_PATH" submodule update --init --recursive
-
-bash "$ROOT"/build/write_git_metadata.sh "$EXT_PATH" "$EXTENSION_COMMIT_HASH_FILE" "$EXTENSION_COMMIT_HASH_VARIABLE"
-
-# remove git things from release package
-rm "$EXT_PATH"/.git* -rfv
+bash "$ROOT"/build/clean_repo.sh "$EXT_PATH"
 
 cp -r "$EXT_PATH" "$FINAL_EXTENSION_PATH"
