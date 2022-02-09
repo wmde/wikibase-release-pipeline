ac06e979d5b5a49e74a56b60edf9ef2ca42ebfbf
Tobias <tobias.andersson@wikimedia.de>
2022-02-09 10:07:25 +0100

    Fix QuickStatements example (#289)
    * Fix QuickStatements example
    
    - Move public env var to .env
    - Add testing
    
    * use localhost for QS public in template

--- test/test_example.sh
+++ test/test_example.sh
@@ -17,6 +17,6 @@ set -o allexport; source ../example/template.env; source example.env; set +o all
 export DATABASE_IMAGE_NAME="$MYSQL_IMAGE_NAME"
 
 ## Use in combination with example compose files 
-export DEFAULT_SUITE_CONFIG="--env-file ../example/template.env -f ../example/docker-compose.yml -f ../example/docker-compose.extra.yml -f docker-compose.example.yml"
+export DEFAULT_SUITE_CONFIG="-f ../example/docker-compose.yml -f ../example/docker-compose.extra.yml -f docker-compose.example.yml"
 
 bash run_selenium.sh "$SUITE"
