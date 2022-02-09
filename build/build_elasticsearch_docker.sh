fb84e9921e1935d298cceb189b568564e6eb0d32
addshore <addshorewiki@gmail.com>
2022-02-09 15:58:17 +0000

    Undo my ELASTICSEARCH_PLUGIN_VERSION mess (not needed)

--- build/build_elasticsearch_docker.sh
+++ build/build_elasticsearch_docker.sh
@@ -4,7 +4,6 @@ set -e
 echo "Building elasticseach $ELASTICSEARCH_VERSION"
 docker build \
     --build-arg=ELASTICSEARCH_VERSION="$ELASTICSEARCH_VERSION" \
-    --build-arg=ELASTICSEARCH_PLUGIN_VERSION="$ELASTICSEARCH_PLUGIN_VERSION" \
     Docker/build/Elasticsearch/ -t "$1"
 
 docker save "$1" | gzip -"$GZIP_COMPRESSION_RATE"f > "$(pwd)"/artifacts/elasticsearch.docker.tar.gz
