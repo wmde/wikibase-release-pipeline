b53d1f2ac47863a66d6e2738a0afb8c0d8a27f6d
addshore <addshorewiki@gmail.com>
2022-02-10 20:03:53 +0000

    Only load CirrusSearch extensions if MW_ELASTIC_HOST is set
    Bug: T299393

--- Docker/build/WikibaseBundle/LocalSettings.d.template/WikibaseCirrusSearch.php
+++ Docker/build/WikibaseBundle/LocalSettings.d.template/WikibaseCirrusSearch.php
@@ -1,9 +1,8 @@
 <?php
 
 // https://www.mediawiki.org/wiki/Extension:WikibaseCirrusSearch
-wfLoadExtension( 'WikibaseCirrusSearch' );
-
 if ( getenv('MW_ELASTIC_HOST') !== false ) {
+    wfLoadExtension( 'WikibaseCirrusSearch' );
     $wgCirrusSearchServers = [ $_ENV['MW_ELASTIC_HOST'] ];
     $wgSearchType = 'CirrusSearch';
     $wgCirrusSearchExtraIndexSettings['index.mapping.total_fields.limit'] = 5000;
