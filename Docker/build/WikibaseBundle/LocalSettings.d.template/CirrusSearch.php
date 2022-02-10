9ecb1142539a4b0369c330081f12cd3b63865677
addshore <addshorewiki@gmail.com>
2022-02-10 20:03:53 +0000

    Only load CirrusSearch extensions if MW_ELASTIC_HOST is set
    Bug: T299393

--- Docker/build/WikibaseBundle/LocalSettings.d.template/CirrusSearch.php
+++ Docker/build/WikibaseBundle/LocalSettings.d.template/CirrusSearch.php
@@ -1,6 +1,8 @@
 <?php
 
 // https://www.mediawiki.org/wiki/Extension:CirrusSearch
-wfLoadExtension( 'CirrusSearch' );
+if ( getenv('MW_ELASTIC_HOST') !== false ) {
+    wfLoadExtension( 'CirrusSearch' );
+}
 
 // See WikibaseCirrusSearch.php for further configuration
