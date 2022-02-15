5f8baa1909f2a56a712daea8d8444f17b8cbd502
addshore <addshore@users.noreply.github.com>
2022-02-15 11:19:06 +0000

    docker run --rm for publish steps (#310)
    Without this if I run the dockerhub publish step
    (or any other publish step) ~42GB of my disk space
    gets used due to the dind conteext being a docker
    volume that will never be auto cleaned.

--- publish/tar.sh
+++ publish/tar.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd publish
-docker-compose build download_artifacts && docker-compose run download_artifacts
-docker-compose build upload_tar && docker-compose run upload_tar --rm
+docker-compose build download_artifacts && docker-compose run --rm download_artifacts
+docker-compose build upload_tar && docker-compose run --rm upload_tar
