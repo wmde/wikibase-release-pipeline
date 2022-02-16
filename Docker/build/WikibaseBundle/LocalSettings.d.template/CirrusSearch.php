b8aa96cb0cd99054631b558535ef1f3a9b8d41b8
addshore <addshore@users.noreply.github.com>
2022-02-16 13:29:16 +0000

    Make a wmde.6 release (from main) (#311)
    * Fix upgrade
    
    The env vars from the image being tested was not properly set when
    testing leading to the wrong templates being used.
    
    * Update docs
    
    * Update test/test_upgrade.sh
    
    * wmde.6 release
    
    * Revert "Only load CirrusSearch extensions if MW_ELASTIC_HOST is set"
    
    This reverts commit b53d1f2ac47863a66d6e2738a0afb8c0d8a27f6d.
    
    Reverting this so that we can make a wmde.6 release that functionally
    is the same as wmde.5.
    
    Will revert the revert after the release is done...
    
    * wmde6 Build and test Wikibase and friends
    
    * Revert "wmde6 Build and test Wikibase and friends"
    
    This reverts commit 41f5f811000c516657f9384caaf6a5c667a2d460.
    
    * Revert "Revert "wmde6 Build and test Wikibase and friends""
    
    This reverts commit 2dbc824131ce5d90529a423efba71cfc84f98834.
    
    Co-authored-by: toan <tobias.andersson@wikimedia.de>

--- Docker/build/WikibaseBundle/LocalSettings.d.template/CirrusSearch.php
+++ Docker/build/WikibaseBundle/LocalSettings.d.template/CirrusSearch.php
@@ -1,8 +1,6 @@
 <?php
 
 // https://www.mediawiki.org/wiki/Extension:CirrusSearch
-if ( getenv('MW_ELASTIC_HOST') !== false ) {
-    wfLoadExtension( 'CirrusSearch' );
-}
+wfLoadExtension( 'CirrusSearch' );
 
 // See WikibaseCirrusSearch.php for further configuration
