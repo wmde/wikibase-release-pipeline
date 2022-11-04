7867c78b22e37c037f40e46025c028fa92c8a70a
addshore <addshorewiki@gmail.com>
2022-11-04 12:48:35 -0100

    Don't run dispatchChanges maint script

--- test/config/client/wikibase/extra-install-repo.sh
+++ test/config/client/wikibase/extra-install-repo.sh
@@ -6,8 +6,5 @@ set -ex
 # repo
 php /var/www/html/maintenance/addSite.php --wiki my_wiki --conf LocalSettings.php  --pagepath=http://wikibase-client.svc/wiki/\$1  --filepath=http://wikibase-client.svc/w/\$1 --language en --interwiki-id client_wiki client_wiki mywikigroup
 
-## run dispatcher
-php /var/www/html/extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki my_wiki --idle-delay 5 --dispatch-interval 1 --client client_wiki &
-
 # startup jobs
 php /var/www/html/maintenance/runJobs.php --wiki my_wiki --wait &
