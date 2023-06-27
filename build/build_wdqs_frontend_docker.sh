6f553d9286240a18fc6b6a28f1589284b44e468f
Loren Johnson <lorenjohnson@gmail.com>
2023-06-27 12:03:45 +0200

    Pin platform to amd64 for WDQS. Fixes an issue with Chromium install on M1 Mac

--- build/build_wdqs_frontend_docker.sh
+++ build/build_wdqs_frontend_docker.sh
@@ -2,6 +2,6 @@
 set -e
 
 cp -r "$TARBALL_PATH" Docker/build/WDQS-frontend
-docker build --pull --build-arg tarball="$(basename "$TARBALL_PATH")" Docker/build/WDQS-frontend/ -t "$1"
+docker build --platform=linux/amd64 --pull --build-arg tarball="$(basename "$TARBALL_PATH")" Docker/build/WDQS-frontend/ -t "$1"
 
 docker save "$1" | gzip -"$GZIP_COMPRESSION_RATE"f > "$(pwd)"/artifacts/"$1".docker.tar.gz
