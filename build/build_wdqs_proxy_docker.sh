e7a8c6f6aecdef2328653d6ffb6d74a5f69bdc75
rti <rti@users.noreply.github.com>
2023-08-01 19:55:14 +0700

    T341222: extend gzip compression rate usage (#421)
    * feat: extend gzip compression rate usage
    
    The release pipeline has a setting called GZIP_COMPRESSION_RATE that can
    be set in local.env. Using this setting gzip invocations are configured.
    The user can choose the compression rate/speed tradeoff. 1 being fast
    and weak compression, 9 being slow and strong compression. This change
    extends the usage of this configuration value to the remaining gzip
    invocations as well as fixes the handing over of the actual value to
    gzip by removing the extra 'f' (not required or supported according to
    man gzip(1)).

--- build/build_wdqs_proxy_docker.sh
+++ build/build_wdqs_proxy_docker.sh
@@ -4,4 +4,4 @@ set -ex
 docker build \
     Docker/build/WDQS-proxy/ -t "$1"
 
-docker save "$1" | gzip -"$GZIP_COMPRESSION_RATE"f > artifacts/"$1".docker.tar.gz
+docker save "$1" | gzip -"$GZIP_COMPRESSION_RATE" > artifacts/"$1".docker.tar.gz
