32a2e86459abb0f98027daa8d5d190e172594d6d
Loren Johnson <lorenjohnson@gmail.com>
2023-08-03 17:49:28 +0200

    Improve check_if_up() / curl test logging

--- Docker/test/curl/test_curl.sh
+++ Docker/test/curl/test_curl.sh
@@ -5,15 +5,21 @@ check_if_up() {
     PATH=$2
 
     FULL_URL="$1$2"
+    DISPLAY_URL="http://$FULL_URL"
+
 
     if [ -z "$URL" ]; then
+        echo "❌ check_if_up called with an empty URL (with PATH: $2)"
+
         return
     fi
 
-    if /usr/bin/curl --fail --retry 120 --retry-all-errors --retry-delay 1 --max-time 10 --retry-max-time 120 --show-error --output /dev/null --silent "$FULL_URL"; then
-        echo "Successfully loaded $URL!"
+    echo "🔄 Waiting for $DISPLAY_URL"
+
+    if /usr/bin/curl --fail --retry 120 --retry-all-errors --retry-delay 1 --max-time 10 --retry-max-time 120 --output /dev/null --silent "$FULL_URL"; then
+        echo "✅ Successfully loaded $DISPLAY_URL!"
     else
-        echo "Could not retrieve $URL"
+        echo "❌ Could not load $DISPLAY_URL"
         exit 1
     fi
 }
