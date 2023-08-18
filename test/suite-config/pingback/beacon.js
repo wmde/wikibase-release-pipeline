f43a9e2ab7fdb72a0f703bdf867243067d6ecac7
Loren Johnson <lorenjohnson@gmail.com>
2023-08-18 11:01:34 +0200

    Linting

--- test/suite-config/pingback/beacon.js
+++ test/suite-config/pingback/beacon.js
@@ -1,4 +1,4 @@
-/* eslint-disable node/no-deprecated-api */		
+/* eslint-disable node/no-deprecated-api */
 'use strict';
 
 const http = require( 'http' );
@@ -9,7 +9,8 @@ console.log( 'beacon started!' );
 const messages = [];
 
 http.createServer( function ( req, res ) {
-	// TODO: 'url.parse' was deprecated since v11.0.0. Use 'url.URL' constructor instead (then re-enable linting rule above)
+	// TODO: 'url.parse' was deprecated since v11.0.0. Use 'url.URL'
+	// constructor instead (then re-enable linting rule above)
 	const queryObject = url.parse( req.url, true ).query;
 	messages.push( queryObject );
 
