5389d8a6e73b503812ab7c4fdc598d8954f7e270
Loren Johnson <lorenjohnson@gmail.com>
2023-08-19 22:24:29 +0200

    Doesn't load Elasticsearch related extensions when MW_ELASTIC is not set. Stops a lot of error logging.

--- Docker/build/WikibaseBundle/LocalSettings.d.template/WikibaseCirrusSearch.php
+++ Docker/build/WikibaseBundle/LocalSettings.d.template/WikibaseCirrusSearch.php
@@ -1,9 +1,9 @@
 <?php
 
-// https://www.mediawiki.org/wiki/Extension:WikibaseCirrusSearch
-wfLoadExtension( 'WikibaseCirrusSearch' );
-
 if ( getenv('MW_ELASTIC_HOST') !== false ) {
+    // https://www.mediawiki.org/wiki/Extension:WikibaseCirrusSearch
+    wfLoadExtension( 'WikibaseCirrusSearch' );
+
     $wgCirrusSearchServers = [ $_ENV['MW_ELASTIC_HOST'] ];
     $wgSearchType = 'CirrusSearch';
     $wgCirrusSearchExtraIndexSettings['index.mapping.total_fields.limit'] = 5000;
