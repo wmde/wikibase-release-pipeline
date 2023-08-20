e9617cc69076c3b651cc821319def293d0b15a3d
Loren Johnson <lorenjohnson@gmail.com>
2023-08-20 09:55:44 +0200

    Wikibase, Client, and Repo MediaWiki logs now go to log/SUITE/mw.debug.log, log/SUITE/mw.client.debug.log, etc. SQL trace and dumps excluded from MediaWiki logs in all cases (partially addresses T343355)

--- test/suite-config/LocalSettings.php
+++ test/suite-config/LocalSettings.php
@@ -2,13 +2,13 @@
 ini_set( 'display_errors', 1 );
 $wgShowExceptionDetails = true;
 $wgShowSQLErrors = true;
-$wgDebugDumpSql  = true;
-$wgShowDBErrorBacktrace = true;
-$wgDebugLogFile = '/var/log/mediawiki/debug.log';
+$wgDebugDumpSql  = false;
+$wgShowDBErrorBacktrace = false;
+$wgDebugLogFile = '/var/log/mediawiki/mw.debug.log';
 
-// Tone-down debug settings?
-// $wgShowExceptionDetails = true;
-// $wgShowSQLErrors = true;
-// $wgDebugDumpSql  = false;
-// $wgShowDBErrorBacktrace = false;
-// $wgDebugLogFile = '/var/log/mediawiki/debug.log';
+$wgDebugLogGroups = array(
+	'resourceloader' => '/var/log/mediawiki/mw.resourceloader.log',
+	'exception' => '/var/log/mediawiki/mw.exception.log',
+	'error' => '/var/log/mediawiki/mw.error.log',
+	'fatal' => '/var/log/mediawiki/mw.fatal.log',
+);
