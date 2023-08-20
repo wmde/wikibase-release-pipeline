1d887069a2d1b642515f336831bfc1033c283c05
Loren Johnson <lorenjohnson@gmail.com>
2023-08-20 12:42:31 +0200

    Make JSON Reporter a bit more versatile, and include start time of test run

--- test/helpers/json-reporter.js
+++ test/helpers/json-reporter.js
@@ -11,45 +11,61 @@ class JsonReporter extends reporter.default {
 		super( options );
 
 		this.resultFilePath = options.resultFilePath;
+		this.suite = {};
 		this.failures = [];
 		this.success = [];
 		this.skipped = [];
 	}
+
 	onTestSkip( test ) {
 		this.skipped.push( {
 			fullTitle: test.fullTitle
 		} );
 	}
+
 	onTestPass( test ) {
 		this.success.push( {
 			fullTitle: test.fullTitle
 		} );
 	}
+
 	onTestFail( test ) {
 		this.failures.push( {
 			fullTitle: test.fullTitle,
 			error: test.error
 		} );
 	}
-	onRunnerEnd() {
+
+	onSuiteEnd( suiteStats ) {
+		const suite = process.env.SUITE;
 		const result = {
-			fail: this.failures,
-			pass: this.success,
-			skip: this.skipped
+			[suite]: {
+				fail: this.failures,
+				pass: this.success,
+				skip: this.skipped
+			}
 		};
 
 		if ( fs.existsSync( this.resultFilePath ) ) {
 			const existing = JSON.parse( fs.readFileSync( this.resultFilePath, 'utf8' ) );
 
+			result.start = suiteStats.start;
+
 			if ( existing ) {
-				result.pass = result.pass.concat( existing.pass );
-				result.fail = result.fail.concat( existing.fail );
-				result.skip = result.skip.concat( existing.skip );
+				result.start = existing.start ? existing.start : result.start;
+				result[suite].pass = result[suite].pass.concat( existing[suite].pass );
+				result[suite].fail = result[suite].fail.concat( existing[suite].fail );
+				result[suite].skip = result[suite].skip.concat( existing[suite].skip );
+			} else {
+			// 	// Capture the start time of the entire suite, not just the test file
+				console.log('!!! no existing file')
+			// 	result[suite].start = suiteStats.start;
 			}
 		}
 
 		fs.writeFileSync( this.resultFilePath, JSON.stringify( result, null, 2 ), 'utf-8' );
 	}
+
 }
 
 module.exports = JsonReporter;
