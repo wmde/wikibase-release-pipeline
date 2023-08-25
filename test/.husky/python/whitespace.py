945c18008ef0085d2b894d12128c03f6e68c98f8
Ricki Jay <ricki.jay@wikimedia.de>
2023-08-25 11:15:02 +0200

    Reorg

--- test/.husky/python/whitespace.py
+++ test/.husky/python/whitespace.py
@@ -1,30 +1,41 @@
-import re
 import glob
 import os
 
-trailing_whitespace = re.compile(r"([ \t\r]+)(\n)")
 root_dir = os.path.join(os.getcwd(), "..", "..")
 
-for file in glob.glob("**/*.(js|json|jsx|ts|tsx|py)", root_dir= root_dir, recursive=True):
-    if "node_modules" not in file:
-        file_path = os.path.join(root_dir, file)
-        temp_file_path = os.path.join(root_dir, "temp", file)
-        temp_file_dir = os.path.dirname(temp_file_path)
+print(root_dir)
 
-        replacing = False
-        with open(file_path, mode='r+') as current_file:
-            contents = current_file.read()
-            # while re.search(trailing_whitespace, contents):
-            #     contents = re.sub(trailing_whitespace, r"\2", contents)
-            #     replacing = True
-            if not contents.endswith('\n'):
-                contents += "\n"
-                replacing = True
-            if replacing:
-                os.makedirs(temp_file_dir)
-                with open(temp_file_path, mode='w+') as new_file:
-                    new_file.write(contents)
+
+def add_newline(file: str, root_dir: str):
+    file_path = os.path.join(root_dir, file)
+    temp_file_path = os.path.join(root_dir, "temp", file)
+    temp_file_dir = os.path.dirname(temp_file_path)
+
+    replacing = False
+    with open(file_path, mode='r') as current_file:
+        contents = current_file.read()
+        if not contents.endswith('\n'):
+            contents += "\n"
+            replacing = True
         if replacing:
+            os.makedirs(temp_file_dir)
+            with open(temp_file_path, mode='w+') as new_file:
+                new_file.write(contents)
+    if replacing:
+        try:
             os.remove(file_path)
             os.rename(temp_file_path, file_path)
+            print(file)
+        except PermissionError:
+            os.remove(temp_file_path)
+        finally:
             os.removedirs(temp_file_dir)
+
+
+extensions = ["js", "jsx", "ts", "tsx", "sh", "json"]
+
+
+for ext in extensions:
+    for file in  glob.glob(f"**/*.{ext}", root_dir=root_dir, recursive=True) :
+        if "node_modules" not in file:
+            add_newline(file, root_dir)
