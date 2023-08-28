a06d11d988dcf9204be2ca3736574b09fa2cfb36
Loren Johnson <lorenjohnson@gmail.com>
2023-08-28 12:50:43 +0200

    Run linting only in Docker container, and quiet status messages

--- lint.sh
+++ lint.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
 
-# Dockerfile linting with Hadolint (uses rules from `.hadolint`)
+cd test
+
+# echo "ℹ️  Linting Dockerfiles (**/Dockerfile)"
 # https://github.com/hadolint/hadolint
-# -type d -name node_modules -prune
-echo "ℹ️  Linting Dockerfiles (**/Dockerfile)"
 docker run --rm -v "$(pwd)":/code -v ./.hadolint.yml:/.hadolint.yml hadolint/hadolint:latest-alpine sh -c "find /code -type f -name 'Dockerfile' | xargs hadolint"
 
-# Bash Script linting with Spellcheck (bash)
+# echo "ℹ️  Linting Shell Scripts (**/*.sh)"
 # https://github.com/koalaman/shellcheck#from-your-terminal
-echo "ℹ️  Linting Shell Scripts (**/*.sh)"
 find . -type d -name node_modules -prune -false -o -name "*.sh" -print0 | xargs -0 docker run --rm -v "$(pwd)":/code dcycle/shell-lint:2
 
-# Javascript linting Selenium tests
-echo "ℹ️  Linting Javascript in test (test/**/.js)"
-{
-  cd test
-  npm install --loglevel=error
-} > /dev/null || exit
-npm run lint --silent
-cd ..  > /dev/null || exit
+# echo "ℹ️  Linting Javascript (test/**/.js and docs/diagrams/**/.js)"
+docker compose run --rm --volume "${PWD}/../docs/diagrams:/tmp/diagrams" test-runner -c "
+  npm run lint --silent &&
+  cd /tmp/diagrams
+  npm install --loglevel=error --progress=false --no-audit --no-fund > /dev/null &&
+  npm run lint --silent
+"
 
-# Javascript linting Diagrams code
-echo "ℹ️  Linting Javascript in Diagrams code (docs/diagrams/**/.js)"
-{
-  cd docs/diagrams
-  npm install  --loglevel=error
-} > /dev/null || exit
-npm run lint --silent
 cd ..  > /dev/null || exit
