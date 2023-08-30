75d598e2a8da665e424431e676c26551aae91cd2
Loren Johnson <lorenjohnson@gmail.com>
2023-08-30 13:57:56 +0200

    Remove unnecessary cd / path management in linting script

--- test/scripts/lint.sh
+++ test/scripts/lint.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-cd test
-
-# ℹ️  Linting Dockerfiles (**/Dockerfile)
+# ℹ️ Linting Dockerfiles (**/Dockerfile)
 # https://github.com/hadolint/hadolint
-docker run --rm -v "$(pwd)/..":/code -v "$(pwd)/../.hadolint.yml":/.hadolint.yml hadolint/hadolint:latest-alpine sh -c "find /code -type f -name 'Dockerfile' | xargs hadolint"
+docker run --rm -v "$(pwd)":/code -v "$(pwd)/.hadolint.yml":/.hadolint.yml hadolint/hadolint:latest-alpine sh -c "find /code -type f -name 'Dockerfile' | xargs hadolint"
 
-# ℹ️  Linting Shell Scripts (**/*.sh)
+# ℹ️ Linting Shell Scripts (**/*.sh)
 # https://github.com/koalaman/shellcheck#from-your-terminal
 find . -type d -name node_modules -prune -false -o -name "*.sh" -print0 | xargs -0 docker run --rm -v "$(pwd)":/code dcycle/shell-lint:2
 
-# ℹ️  Linting Javascript (test/**/*.js and docs/diagrams/**/*.js)
-docker compose run --rm --volume "$(pwd)/../docs/diagrams:/tmp/diagrams" test-runner -c "
+# ℹ️ Linting Javascript (test/**/*.js and docs/diagrams/**/*.js)
+docker compose -f test/docker-compose.yml run --rm -v "$(pwd)/docs/diagrams:/tmp/diagrams" test-runner -c "
   npm run lint --silent &&
   cd /tmp/diagrams &&
   npm install --loglevel=error --progress=false --no-audit --no-fund > /dev/null &&
   npm run lint --silent
 "
-
-cd ..  > /dev/null
