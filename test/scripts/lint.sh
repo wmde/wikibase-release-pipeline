97db85fd33a9db9173f7102a0343d8592c15be0e
roti <robert.timm@wikimedia.de>
2023-08-30 13:26:02 +0200

    fix: path to hadolint

--- test/scripts/lint.sh
+++ test/scripts/lint.sh
@@ -3,16 +3,16 @@ set -e
 
 cd test
 
-# echo "ℹ️  Linting Dockerfiles (**/Dockerfile)"
+# ℹ️  Linting Dockerfiles (**/Dockerfile)
 # https://github.com/hadolint/hadolint
-docker run --rm -v "$(pwd)":/code -v ./.hadolint.yml:/.hadolint.yml hadolint/hadolint:latest-alpine sh -c "find /code -type f -name 'Dockerfile' | xargs hadolint"
+docker run --rm -v "$(pwd)":/code -v "$(pwd)/../.hadolint.yml":/.hadolint.yml hadolint/hadolint:latest-alpine sh -c "find /code -type f -name 'Dockerfile' | xargs hadolint"
 
-# echo "ℹ️  Linting Shell Scripts (**/*.sh)"
+# ℹ️  Linting Shell Scripts (**/*.sh)
 # https://github.com/koalaman/shellcheck#from-your-terminal
 find . -type d -name node_modules -prune -false -o -name "*.sh" -print0 | xargs -0 docker run --rm -v "$(pwd)":/code dcycle/shell-lint:2
 
-# echo "ℹ️  Linting Javascript (test/**/*.js and docs/diagrams/**/*.js)"
 docker compose run --rm --volume "${PWD}/../docs/diagrams:/tmp/diagrams" test-runner -c "
+# ℹ️  Linting Javascript (test/**/*.js and docs/diagrams/**/*.js)
   npm run lint --silent &&
   cd /tmp/diagrams
   npm install --loglevel=error --progress=false --no-audit --no-fund > /dev/null &&
