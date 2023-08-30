a102d65deeffe6afa2b8113143578649b1aab24e
Loren Johnson <lorenjohnson@gmail.com>
2023-08-30 15:28:17 +0200

    repo/api spec async converted

--- test/specs/repo/api.js
+++ test/specs/repo/api.js
@@ -6,14 +6,14 @@ const Util = require( 'wdio-mediawiki/Util' );
 
 describe( 'Wikibase API', function () {
 
-	it( 'Should be able to create many properties and items', function () {
+	it( 'Should be able to create many properties and items', async function () {
 
 		const numEntities = 100;
 		for ( let i = 0; i < numEntities; i++ ) {
 
 			const itemLabel = 'T267743-';
 			const propertyValue = 'PropertyExampleStringValue';
-			const propertyId = browser.call( () => WikibaseApi.createProperty( 'string' ) );
+			const propertyId = await WikibaseApi.createProperty( 'string' );
 			const data = {
 				claims: [
 					{
@@ -26,9 +26,7 @@ describe( 'Wikibase API', function () {
 				]
 			};
 
-			const itemId = browser.call(
-				() => WikibaseApi.createItem( Util.getTestString( itemLabel ), data )
-			);
+			const itemId = await WikibaseApi.createItem( Util.getTestString( itemLabel ), data );
 
 			assert.strictEqual( itemId.startsWith( 'Q' ), true );
 			assert.strictEqual( propertyId.startsWith( 'P' ), true );
