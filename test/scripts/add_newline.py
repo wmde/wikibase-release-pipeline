dfaf6336ad4a494909a95a4450f252971bf44c1e
Ricki Jay <ricki.jay@wikimedia.de>
2023-08-31 13:42:34 +0200

    Don't Read Everything

--- test/scripts/add_newline.py
+++ test/scripts/add_newline.py
@@ -5,8 +5,11 @@ def add_newline(file: str, root_dir: str) -> bool:
     file_path = os.path.join(root_dir, file)
 
     with open(file_path, mode='r+') as current_file:
-        contents = current_file.read()
-        if not contents.endswith('\n'):
+        current_file.seek(0, os.SEEK_END)
+        current_file.seek(current_file.tell()-1, os.SEEK_SET)
+
+        contents = current_file.read(1)
+        if not contents.endswith("\n"):
             current_file.write("\n")
             return True
     return False
@@ -14,6 +17,7 @@ def add_newline(file: str, root_dir: str) -> bool:
 
 if __name__ == "__main__":
     extensions = ["js", "jsx", "ts", "tsx", "sh", "json", "py"]
+    exclusions = ["node_modules"]
 
     for root, _, files in os.walk(os.getcwd()):
         if "node_modules" not in root:
