c4288445bc6653e918876a98c630157161708393
Loren Johnson <lorenjohnson@gmail.com>
2023-08-31 17:05:54 +0200

    T343802 Fix: Run linting only in Docker container only, quiet status messages (#458)

---
+++ test/scripts/lint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# ℹ️ Linting Dockerfiles (**/Dockerfile)
+# https://github.com/hadolint/hadolint
+docker run --rm -v "$(pwd)":/code -v "$(pwd)/.hadolint.yml":/.hadolint.yml hadolint/hadolint:latest-alpine sh -c "find /code -type f -name 'Dockerfile' | xargs hadolint"
+
+# ℹ️ Linting Shell Scripts (**/*.sh)
+# https://github.com/koalaman/shellcheck#from-your-terminal
+find . -type d -name node_modules -prune -false -o -name "*.sh" -print0 | xargs -0 docker run --rm -v "$(pwd)":/code dcycle/shell-lint:2
+
+# ℹ️ Linting Javascript (test/**/*.js and docs/diagrams/**/*.js)
+docker compose -f test/docker-compose.yml run --rm -v "$(pwd)/docs/diagrams:/tmp/diagrams" test-runner -c "
+  npm run lint --silent &&
+  cd /tmp/diagrams &&
+  npm install --loglevel=error --progress=false --no-audit --no-fund > /dev/null &&
+  npm run lint --silent
+"
