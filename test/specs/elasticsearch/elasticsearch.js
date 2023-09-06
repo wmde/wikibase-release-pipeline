d6ea1c1cbb8cbd26189d2fd4fefe388e4f69b870
Ricki Jay <ricki.jay@wikimedia.de>
2023-09-06 17:25:15 +0200

    Elasticsearch

--- test/specs/elasticsearch/elasticsearch.js
+++ test/specs/elasticsearch/elasticsearch.js
@@ -8,58 +8,66 @@ const itemAlias = Util.getTestString( 'alias' );
 const itemLabel = Util.getTestString( 'testItem' );
 
 describe( 'ElasticSearch', function () {
-
 	let itemId;
 
-	it( 'Should create an item', function () {
+	it( 'Should create an item', async () => {
+		itemId = await WikibaseApi.createItem( itemLabel );
 
-		itemId = browser.call(
-			() => WikibaseApi.createItem( itemLabel )
+		await browser.url( process.env.MW_SERVER + '/wiki/Item:' + itemId );
+		const addButtonEl = await $(
+			'.wikibase-toolbarbutton.wikibase-toolbar-item.wikibase-toolbar-button.wikibase-toolbar-button-add'
 		);
-
-		browser.url( process.env.MW_SERVER + '/wiki/Item:' + itemId );
-		$( '.wikibase-toolbarbutton.wikibase-toolbar-item.wikibase-toolbar-button.wikibase-toolbar-button-add' ).waitForDisplayed();
+		await addButtonEl.waitForDisplayed();
 	} );
 
-	it( 'Should be able to set alias', function () {
-
-		browser.url( process.env.MW_SERVER + '/wiki/Special:SetAliases/' );
+	it( 'Should be able to set alias', async () => {
+		await browser.url( process.env.MW_SERVER + '/wiki/Special:SetAliases/' );
 
 		// input id
-		$( '#wb-modifyentity-id input' ).waitForDisplayed();
-		$( '#wb-modifyentity-id input' ).setValue( itemId );
+		const inputIdEl = await $( '#wb-modifyentity-id input' );
+		await inputIdEl.waitForDisplayed();
+		await inputIdEl.setValue( itemId );
 
 		// input alias term and submit
-		$( '#wb-modifyterm-value input' ).waitForDisplayed();
-		$( '#wb-modifyterm-value input' ).setValue( itemAlias );
-		$( 'button.oo-ui-inputWidget-input' ).click();
+		const inputValueEl = await $( '#wb-modifyterm-value input' );
+		await inputValueEl.waitForDisplayed();
+		await inputValueEl.setValue( itemAlias );
+
+		const inputWidgetEl = await $( 'button.oo-ui-inputWidget-input' );
+		await inputWidgetEl.click();
 
 		// alias should be visible on item page
-		$( '.wikibase-aliasesview-list-item' ).waitForDisplayed();
-		const alias = $( '.wikibase-aliasesview-list-item' ).getText();
+		const aliasesViewEl = await $( '.wikibase-aliasesview-list-item' );
+		await aliasesViewEl.waitForDisplayed();
+		const alias = await aliasesViewEl.getText();
 		assert.strictEqual( alias, itemAlias );
 	} );
 
-	it( 'should be able to search case-insensitive', async function () {
-
+	it( 'should be able to search case-insensitive', async () => {
 		let searchResult;
 
 		await browser.waitUntil(
 			async () => {
-				const resp = await browser.makeRequest( process.env.MW_SERVER + '/w/api.php?action=wbsearchentities&search=Test&format=json&errorformat=plaintext&language=en&uselang=en&type=item' );
+				const resp = await browser.makeRequest(
+					process.env.MW_SERVER +
+					'/w/api.php?action=wbsearchentities&search=Test&format=json&errorformat=plaintext&language=en&uselang=en&type=item'
+				);
 				searchResult = resp.data.search;
 
-				return searchResult.length === 1 &&
+				return (
+					searchResult.length === 1 &&
 					searchResult[ 0 ].id === itemId &&
 					searchResult[ 0 ].match.type === 'label' &&
-					searchResult[ 0 ].match.text === itemLabel;
+					searchResult[ 0 ].match.text === itemLabel
+				);
 			},
 			{
 				timeout: 20000,
 				timeoutMsg: 'Elasticsearch should have updated the label by now.'
 			}
 		);
-		assert( searchResult.length === 1 &&
+		assert(
+			searchResult.length === 1 &&
 			searchResult[ 0 ].id === itemId &&
 			searchResult[ 0 ].match.type === 'label' &&
 			searchResult[ 0 ].match.text === itemLabel
@@ -67,18 +75,22 @@ describe( 'ElasticSearch', function () {
 	} );
 
 	it( 'should be able to search via alias', async function () {
-
 		let searchResult;
 
 		await browser.waitUntil(
 			async () => {
-				const resp = await browser.makeRequest( process.env.MW_SERVER + '/w/api.php?action=wbsearchentities&search=alias&format=json&errorformat=plaintext&language=en&uselang=en&type=item' );
+				const resp = await browser.makeRequest(
+					process.env.MW_SERVER +
+					'/w/api.php?action=wbsearchentities&search=alias&format=json&errorformat=plaintext&language=en&uselang=en&type=item'
+				);
 				searchResult = resp.data.search;
 
-				return searchResult.length === 1 &&
+				return (
+					searchResult.length === 1 &&
 					searchResult[ 0 ].id === itemId &&
 					searchResult[ 0 ].match.type === 'alias' &&
-					searchResult[ 0 ].match.text === itemAlias;
+					searchResult[ 0 ].match.text === itemAlias
+				);
 			},
 			{
 				timeout: 20000,
@@ -92,6 +104,5 @@ describe( 'ElasticSearch', function () {
 			searchResult[ 0 ].match.type === 'alias' &&
 			searchResult[ 0 ].match.text === itemAlias
 		);
-
 	} );
 } );
