e1005a49f49b93aca04829c0b6848318b43d32cc
Ricki Jay <ricki.jay@wikimedia.de>
2023-09-07 15:27:55 +0200

    Turns out there is a difference

--- test/specs/repo/special-version.js
+++ test/specs/repo/special-version.js
@@ -4,16 +4,18 @@ const assert = require( 'assert' );
 const defaultFunctions = require( '../../helpers/default-functions' );
 
 describe( 'Special:Version', function () {
-
-	it( 'Should contain the correct MediaWiki version', function () {
+	it( 'Should contain the correct MediaWiki version', async function () {
 		if ( process.env.WMDE_RELEASE_VERSION === 'wmdeprerelease' ) {
-			return this.skip();
+			this.skip();
 		}
 
-		browser.url( process.env.MW_SERVER + '/wiki/Special:Version' );
-		const text = $( '#sv-software' ).getText();
-		assert.strictEqual( text.includes( 'MediaWiki\t' + process.env.MEDIAWIKI_VERSION ), true );
-
+		await browser.url( process.env.MW_SERVER + '/wiki/Special:Version' );
+		const softwareEl = await $( '#sv-software' );
+		const text = await softwareEl.getText();
+		assert.strictEqual(
+			text.includes( 'MediaWiki\t' + process.env.MEDIAWIKI_VERSION ),
+			true
+		);
 	} );
 
 	const extensions = {
@@ -25,7 +27,14 @@ describe( 'Special:Version', function () {
 			'WikibaseRepository',
 			'WikibaseManifest'
 		],
-		other: [ 'CLDR', 'CirrusSearch', 'Elastica', 'OAuth', 'Parsoid', 'UniversalLanguageSelector' ],
+		other: [
+			'CLDR',
+			'CirrusSearch',
+			'Elastica',
+			'OAuth',
+			'Parsoid',
+			'UniversalLanguageSelector'
+		],
 		parserhook: [ 'Babel', 'Scribunto' ],
 		editor: [ 'VisualEditor' ],
 		antispam: [ 'ConfirmEdit' ],
@@ -35,25 +44,27 @@ describe( 'Special:Version', function () {
 	// should be disabled for dynamic tests
 	// eslint-disable-next-line mocha/no-setup-in-describe
 	Object.keys( extensions ).forEach( ( extensionPackage ) => {
-
 		extensions[ extensionPackage ].forEach( ( extension ) => {
 			// Get the extension name from the override if available
 			const name = extension;
 
-			it( 'Should contain ' + name + ' extensions', function () {
-
+			it( 'Should contain ' + name + ' extensions', async function () {
 				defaultFunctions.skipIfExtensionNotPresent( this, name );
 
-				browser.url( process.env.MW_SERVER + '/wiki/Special:Version' );
+				await browser.url( process.env.MW_SERVER + '/wiki/Special:Version' );
 
 				// /wiki/Special:Version generate these for each installed extension
-				const elementSelector = $( '#mw-version-ext-' + extensionPackage + '-' + extension.replace( / /g, '_' ) );
-				elementSelector.waitForDisplayed();
-				elementSelector.scrollIntoView();
+				const elementSelector = await $(
+					'#mw-version-ext-' +
+					extensionPackage +
+					'-' +
+					extension.replace( / /g, '_' )
+				);
+				await elementSelector.waitForDisplayed();
+				await elementSelector.scrollIntoView();
 
 				assert( elementSelector.getText() !== null );
 			} );
-
 		} );
 	} );
 } );
