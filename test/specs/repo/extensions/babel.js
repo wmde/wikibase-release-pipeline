a9d4671b6ee42108a48b510a5cbaabf2399c08e6
Ricki Jay <ricki.jay@wikimedia.de>
2023-09-07 15:30:24 +0200

    Babel

--- test/specs/repo/extensions/babel.js
+++ test/specs/repo/extensions/babel.js
@@ -5,21 +5,28 @@ const defaultFunctions = require( '../../../helpers/default-functions' );
 const LoginPage = require( 'wdio-mediawiki/LoginPage' );
 
 describe( 'Babel', function () {
-
-	it( 'Should be able to update the user page with language skills', function () {
-
+	it( 'Should be able to update the user page with language skills', async () => {
 		defaultFunctions.skipIfExtensionNotPresent( this, 'Babel' );
 
-		LoginPage.loginAdmin();
+		await LoginPage.loginAdmin();
 
-		const executionContent = browser.editPage(
+		const executionContent = await browser.editPage(
 			process.env.MW_SERVER,
 			'User:' + process.env.MW_ADMIN_NAME,
 			'{{#babel: sv | en }}'
 		);
 
-		assert.strictEqual( executionContent.includes( 'Den här användaren har svenska som modersmål.' ), true );
-		assert.strictEqual( executionContent.includes( 'This user has a native understanding of English.' ), true );
-
+		assert.strictEqual(
+			executionContent.includes(
+				'Den här användaren har svenska som modersmål.'
+			),
+			true
+		);
+		assert.strictEqual(
+			executionContent.includes(
+				'This user has a native understanding of English.'
+			),
+			true
+		);
 	} );
 } );
