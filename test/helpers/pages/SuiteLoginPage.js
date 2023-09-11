38de3fb9d57cc1b57efd5b84eee5d0facd946e77
Loren Johnson <lorenjohnson@gmail.com>
2023-09-11 10:55:27 +0200

    Add reference to wdio-mediawiki LoginPage override

--- test/helpers/pages/SuiteLoginPage.js
+++ test/helpers/pages/SuiteLoginPage.js
@@ -6,6 +6,7 @@ const Page = require( 'wdio-mediawiki/Page' );
 // which as of version 2.2.0 does not properly await WDIO elements
 // causing failures and otherwise unpredictable results.
 // TODO: Fix upstream code in `wdio-mediawiki/LoginPage`
+// See https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/core/+/refs/heads/wmf/1.40.0-wmf.24/tests/selenium/wdio-mediawiki/LoginPage.js
 class SuiteLoginPage extends Page {
 	get username() { return $( '#wpName1' ); }
 	get password() { return $( '#wpPassword1' ); }
