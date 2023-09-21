7b30bed18dcd0bd69adc2c5c1a55700dd4e15d5a
roti <robert.timm@wikimedia.de>
2023-09-21 14:49:16 +0200

    fix: make build files be owned by system user

--- build/build_wikibase.sh
+++ build/build_wikibase.sh
@@ -3,9 +3,9 @@ set -ex
 
 ROOT="$(pwd)"
 
-TEMP_GIT_DIR="$(mktemp -d)"
-TEMP_TAR_DIR="$(pwd)"/artifacts
-WIKIBASE_PATH="$TEMP_GIT_DIR/Wikibase"
+ARTIFACT_PATH="$(pwd)/artifacts"
+WIKIBASE_PATH="/tmp/Wikibase"
+WIKIBASE_PATH_HOST="${HOST_TMP}/Wikibase"
 
 UPDATE_SUBMODULE=1 bash "$ROOT"/build/clone_repo.sh \
     "$WIKIBASE_COMMIT_HASH" \
@@ -19,29 +19,31 @@ bash "$ROOT"/build/clean_repo.sh "$WIKIBASE_PATH"
 # remove travis build file
 rm "$WIKIBASE_PATH"/.travis.yml -vf
 
-WIKIBASE_DIR="$TEMP_GIT_DIR/Wikibase/"
-COMPOSER_VENDOR="$WIKIBASE_DIR/vendor/"
+COMPOSER_VENDOR="$WIKIBASE_PATH/vendor/"
 
 mkdir "$COMPOSER_VENDOR"
 chmod 777 "$COMPOSER_VENDOR"
 
 # TODO remove the below hack:
 # composer config --no-plugins allow-plugins.composer/installers false
+# Note: we need to provide the host path for setting up the volume, as we
+# are currently already in the context of a container, we cannot just use
+# the current path from the current context
 docker run --rm \
-    --volume "$WIKIBASE_DIR":/tmp/Wikibase \
-    -u "$(id -u "${USER}")":"$(id -g "${USER}")" \
+    --volume $WIKIBASE_PATH_HOST:/tmp/Wikibase \
+    -u "$(id -u)":"$(id -g)" \
     "$COMPOSER_IMAGE_NAME:$COMPOSER_IMAGE_VERSION" \
     config --no-plugins allow-plugins.composer/installers false -d "/tmp/Wikibase"
 docker run --rm \
-    --volume "$WIKIBASE_DIR":/tmp/Wikibase \
-    -u "$(id -u "${USER}")":"$(id -g "${USER}")" \
+    --volume "$WIKIBASE_PATH_HOST":/tmp/Wikibase \
+    -u "$(id -u)":"$(id -g)" \
     "$COMPOSER_IMAGE_NAME:$COMPOSER_IMAGE_VERSION" \
     install --no-dev --ignore-platform-reqs -vv -d "/tmp/Wikibase"
 chmod 755 "$COMPOSER_VENDOR"
 
-tar -C "$TEMP_GIT_DIR" -cf - Wikibase | gzip -"$GZIP_COMPRESSION_RATE" > "$TEMP_TAR_DIR"/wikibase.tar.gz
+tar -C $(dirname "$WIKIBASE_PATH") -cf - Wikibase | gzip -"$GZIP_COMPRESSION_RATE" > "$ARTIFACT_PATH"/wikibase.tar.gz
 
-TARBALL_PATH="$TEMP_TAR_DIR/wikibase.tar.gz"
+TARBALL_PATH="$ARTIFACT_PATH/wikibase.tar.gz"
 
 if [ -n "$GITHUB_ENV" ]; then
     echo "TARBALL_PATH=$TARBALL_PATH" >> "$GITHUB_ENV"
