ef1553de0b114c7931f8ae09a0e943b5f9d79f2f
Ricki Jay <ricki.jay@wikimedia.de>
2023-10-06 00:35:43 +0200

    Format

--- test/specs/repo/api.js
+++ test/specs/repo/api.js
@@ -23,7 +23,10 @@ describe( 'Wikibase API', function () {
 				]
 			};
 
-			const itemId = await WikibaseApi.createItem( getTestString( itemLabel ), data );
+			const itemId = await WikibaseApi.createItem(
+				getTestString( itemLabel ),
+				data
+			);
 
 			assert.strictEqual( itemId.startsWith( 'Q' ), true );
 			assert.strictEqual( propertyId.startsWith( 'P' ), true );
