70a0416c95f066ad2aa11dd21e0c3358e45bc3ca
Ricki Jay <ricki.jay@wikimedia.de>
2023-10-16 10:43:22 +0200

    DOCs

--- test/helpers/types/wdio.d.ts
+++ test/helpers/types/wdio.d.ts
@@ -7,28 +7,62 @@ declare namespace WebdriverIO {
 	type LuaCPUValue = import( './lua-cpu-value.js' ).default;
 
 	interface Browser {
+		/**
+		 * Execute query on database
+		 */
 		dbQuery: ( query: string, config?: DatabaseConfig ) => Promise<string>;
+
+		/**
+		 * Delete a claim by guid or pipe-separated list of guids
+		 */
 		deleteClaim: ( claimGuid: string ) => Promise<BotResponse>;
+
+		/**
+		 * Execute docker command on container and get output
+		 */
 		dockerExecute: (
 			container: string,
 			command: string,
 			opts?: string,
 			shouldLog?: boolean,
 		) => Promise<unknown>;
+
+		/**
+		 * Creates or edits a page with content
+		 */
 		editPage: (
 			host: string,
 			title: string,
 			content: Buffer | string,
 			captcha?: string,
 		) => Promise<string>;
+
+		/**
+		 * Execute quickstatements query
+		 */
 		executeQuickStatement: ( theQuery: string ) => Promise<void>;
+
+		/**
+		 * Moves browser to recent changes then asserts that a change is in the api result
+		 */
 		getDispatchedExternalChange: (
 			host: string,
 			expectedChange: ExternalChange,
 		) => Promise<ExternalChange | null>;
+
+		/**
+		 * Get installed extensions on wiki
+		 */
 		getInstalledExtensions: ( server: string ) => Promise<string[] | undefined>;
+
+		/**
+		 * Makes a request to a page and returns the lua cpu profiling data
+		 */
 		getLuaCpuTime: ( host: string, page: string ) => Promise<LuaCPUValue>;
 
+		/**
+		 * Make a get request to get full request response
+		 */
 		makeRequest: (
 			url: string,
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -36,7 +70,12 @@ declare namespace WebdriverIO {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			postData?: any,
 		) => Promise<AxiosResponse>;
+
+		/**
+		 * Query blazegraph directly (only works if proxy is disabled, used in upgrade test)
+		 */
 		queryBlazeGraphItem: ( itemId: string ) => Promise<Binding[]>;
+
 		waitForJobs: (
 			serverURL?: string,
 			timeout?: number,
