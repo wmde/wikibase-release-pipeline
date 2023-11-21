79c7a8d0b7231863a458b22a0650c79dc85a9a99
Loren Johnson <lorenjohnson@gmail.com>
2023-11-21 18:49:09 +0100

    Provisional test script

--- test.sh
+++ test.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
 set -e
 
 cd test
 
-export SUITE=$1
+TEST_RUNNER_COMPOSE="docker compose --env-file ../variables.env --env-file default.env --env-file ../local.env -f docker-compose.yml"
+
+# check if command line argument is empty or not present
+if [ -z $1 ]; then
+        echo 'Please enter either a suite name, or "down" to take down services'
+        exit 0
+elif [ "$1" == 'down' ]; then
+        $TEST_RUNNER_COMPOSE down --volumes
+        exit 0
+else
+        export SUITE=$1
+fi
 
 if [ -z "$SUITE" ]; then
     echo "🚨 SUITE is not set"
     exit 1
 fi
 
-TEST_RUNNER_COMPOSE="docker compose --env-file ../variables.env --env-file default.env --env-file ../local.env -f docker-compose.yml"
 $TEST_RUNNER_COMPOSE up -d
 
 WDIO_COMMAND='npm run test:run --silent'
 
 if [ -n "$FILTER" ]; then
     WDIO_COMMAND='npm run test:run-filter --silent'
+fi
+
+if [[ -n "${HEADED_TESTS}" ]]; then
+    echo "💻 Open http://localhost:7900/?autoconnect=1&resize=scale&password=secret to observe headed tests." 2>&1 | tee -a "$TEST_LOG"
 fi
 
 $TEST_RUNNER_COMPOSE run --rm test-runner -c "$WDIO_COMMAND"
