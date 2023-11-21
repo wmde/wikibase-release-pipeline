481901d338f117f904aa83e1dc29558375bd9798
Loren Johnson <lorenjohnson@gmail.com>
2023-11-21 09:11:59 +0100

    Improve proprety test

--- test/specs/repo/property.ts
+++ test/specs/repo/property.ts
@@ -3,36 +3,75 @@ import WikibaseApi from 'wdio-wikibase/wikibase.api.js';
 import Property from '../../helpers/pages/entity/property.page.js';
 
 describe( 'Property', function () {
-	let propertyId: string = null;
+	let propertyId: string = null, propertyIdSelector: string = null;
+
+	before( async () => {
+		propertyId = await WikibaseApi.getProperty( 'string' );
+		propertyIdSelector = `=${propertyId} (${propertyId})`; // =P1 (P1)
+	} );
 
 	it( 'Should be able to add statement and reference to property', async () => {
-		propertyId = await WikibaseApi.getProperty( 'string' );
-
-		const propertyIdSelector = `=${propertyId} (${propertyId})`; // =P1 (P1)
-
 		await Property.open( propertyId );
 		await Property.addStatement.click();
 
 		// fill out property id for statement
 		await browser.keys( propertyId.split( '' ) );
 		await $( propertyIdSelector ).click();
-		await browser.keys( [ 'S', 'T', 'A', 'T', 'E', 'M', 'E', 'N', 'T' ] );
+		await browser.keys( 'STATEMENT' );
 
 		// wait for save button to re-enable
+		// eslint-disable-next-line wdio/no-pause
 		await browser.pause( 1000 * 1 );
 		await Property.save.click();
+		// eslint-disable-next-line wdio/no-pause
 		await browser.pause( 1000 * 2 );
 
 		await Property.addReference.click();
 
 		// fill out property id for reference
 		await $( '.ui-entityselector-input' );
+		// eslint-disable-next-line wdio/no-pause
 		await browser.pause( 1000 * 1 );
 		await browser.keys( propertyId.split( '' ) );
 		// await $( propertyIdSelector ).click();
 		await $( propertyIdSelector ).click();
-		await browser.keys( [ 'R', 'E', 'F', 'E', 'R', 'E', 'N', 'C', 'E' ] );
+		await browser.keys( 'REFERENCE' );
 
+		// eslint-disable-next-line wdio/no-pause
+		await browser.pause( 1000 * 1 );
+		await Property.save.click();
+
+		await Property.open( propertyId );
+	} );
+
+	it( 'Should be show-up in History for the related entity', async () => {
+		await Property.open( propertyId );
+		await Property.addStatement.click();
+
+		// fill out property id for statement
+		await browser.keys( propertyId.split( '' ) );
+		await $( propertyIdSelector ).click();
+		await browser.keys( 'STATEMENT' );
+
+		// wait for save button to re-enable
+		// eslint-disable-next-line wdio/no-pause
+		await browser.pause( 1000 * 1 );
+		await Property.save.click();
+		// eslint-disable-next-line wdio/no-pause
+		await browser.pause( 1000 * 2 );
+
+		await Property.addReference.click();
+
+		// fill out property id for reference
+		await $( '.ui-entityselector-input' );
+		// eslint-disable-next-line wdio/no-pause
+		await browser.pause( 1000 * 1 );
+		await browser.keys( propertyId.split( '' ) );
+		// await $( propertyIdSelector ).click();
+		await $( propertyIdSelector ).click();
+		await browser.keys( 'REFERENCE' );
+
+		// eslint-disable-next-line wdio/no-pause
 		await browser.pause( 1000 * 1 );
 		await Property.save.click();
 
