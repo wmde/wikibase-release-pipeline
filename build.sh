4f91dfaff8bf1c4dba120db0d05055f780b5b41f
roti <robert.timm@wikimedia.de>
2023-11-22 10:17:38 +0000

    wip: build.sh

--- build.sh
+++ build.sh
@@ -15,65 +15,91 @@ set +o allexport
 # TODO: how do we tag actually?
 #
 function build_wikibase {
-		docker build \
-				--build-arg COMPOSER_IMAGE="$COMPOSER_IMAGE" \
-				--build-arg GIT_IMAGE="$GIT_IMAGE" \
-				--build-arg MEDIAWIKI_IMAGE="$MEDIAWIKI_IMAGE" \
-				--build-arg GIT_CURRENT_REVISION="$(git rev-parse HEAD)" \
-				\
-				--build-arg MEDIAWIKI_SETTINGS_TEMPLATE_FILE="$MEDIAWIKI_SETTINGS_TEMPLATE_FILE" \
-				\
-				--build-arg MW_SITE_NAME="$MW_SITE_NAME" \
-				--build-arg MW_SITE_LANG="$MW_SITE_LANG" \
-				--build-arg MW_WG_JOB_RUN_RATE="$MW_WG_JOB_RUN_RATE" \
-				--build-arg MW_WG_ENABLE_UPLOADS="$MW_WG_ENABLE_UPLOADS" \
-				--build-arg MW_WG_UPLOAD_DIRECTORY="$MW_WG_UPLOAD_DIRECTORY" \
-				--build-arg WIKIBASE_PINGBACK="$WIKIBASE_PINGBACK" \
-				\
-				./Docker/build/Wikibase -t wikibase:${WMDE_RELEASE_VERSION}
+    docker build \
+        --build-arg COMPOSER_IMAGE="$COMPOSER_IMAGE" \
+        --build-arg GIT_IMAGE="$GIT_IMAGE" \
+        --build-arg MEDIAWIKI_IMAGE="$MEDIAWIKI_IMAGE" \
+        --build-arg GIT_CURRENT_REVISION="$(git rev-parse HEAD)" \
+        \
+        --build-arg MEDIAWIKI_SETTINGS_TEMPLATE_FILE="$MEDIAWIKI_SETTINGS_TEMPLATE_FILE" \
+        \
+        --build-arg MW_SITE_NAME="$MW_SITE_NAME" \
+        --build-arg MW_SITE_LANG="$MW_SITE_LANG" \
+        --build-arg MW_WG_JOB_RUN_RATE="$MW_WG_JOB_RUN_RATE" \
+        --build-arg MW_WG_ENABLE_UPLOADS="$MW_WG_ENABLE_UPLOADS" \
+        --build-arg MW_WG_UPLOAD_DIRECTORY="$MW_WG_UPLOAD_DIRECTORY" \
+        --build-arg WIKIBASE_PINGBACK="$WIKIBASE_PINGBACK" \
+        \
+        ./Docker/build/Wikibase -t wikibase:${WMDE_RELEASE_VERSION}
 
-		docker save wikibase:${WMDE_RELEASE_VERSION} | gzip -"$GZIP_COMPRESSION_RATE" > artifacts/wikibase.docker.tar.gz
+    docker save wikibase:${WMDE_RELEASE_VERSION} | gzip -"$GZIP_COMPRESSION_RATE" > artifacts/wikibase.docker.tar.gz
 
-		docker build \
-				--build-arg COMPOSER_IMAGE="$COMPOSER_IMAGE" \
-				--build-arg WMDE_RELEASE_VERSION="$WMDE_RELEASE_VERSION" \
-				\
-				--build-arg BABEL_COMMIT="$BABEL_COMMIT" \
-				--build-arg CLDR_COMMIT="$CLDR_COMMIT" \
-				--build-arg BABEL_COMMIT="$BABEL_COMMIT" \
-				--build-arg CLDR_COMMIT="$CLDR_COMMIT" \
-				--build-arg CIRRUSSEARCH_COMMIT="$CIRRUSSEARCH_COMMIT" \
-				--build-arg CONFIRMEDIT_COMMIT="$CONFIRMEDIT_COMMIT" \
-				--build-arg ELASTICA_COMMIT="$ELASTICA_COMMIT" \
-				--build-arg ENTITYSCHEMA_COMMIT="$ENTITYSCHEMA_COMMIT" \
-				--build-arg NUKE_COMMIT="$NUKE_COMMIT" \
-				--build-arg OAUTH_COMMIT="$OAUTH_COMMIT" \
-				--build-arg SCRIBUNTO_COMMIT="$SCRIBUNTO_COMMIT" \
-				--build-arg SYNTAXHIGHLIGHT_GESHI_COMMIT="$SYNTAXHIGHLIGHT_GESHI_COMMIT" \
-				--build-arg UNIVERSALLANGUAGESELECTOR_COMMIT="$UNIVERSALLANGUAGESELECTOR_COMMIT" \
-				--build-arg VISUALEDITOR_COMMIT="$VISUALEDITOR_COMMIT" \
-				--build-arg WIKIBASECIRRUSSEARCH_COMMIT="$WIKIBASECIRRUSSEARCH_COMMIT" \
-				--build-arg WIKIBASEMANIFEST_COMMIT="$WIKIBASEMANIFEST_COMMIT" \
-				--build-arg WIKIBASEEDTF_COMMIT="$WIKIBASEEDTF_COMMIT" \
-				--build-arg WIKIBASELOCALMEDIA_COMMIT="$WIKIBASELOCALMEDIA_COMMIT" \
-				\
-				./Docker/build/WikibaseBundle -t wikibase-bundle:${WMDE_RELEASE_VERSION}
+    docker build \
+        --build-arg COMPOSER_IMAGE="$COMPOSER_IMAGE" \
+        --build-arg WMDE_RELEASE_VERSION="$WMDE_RELEASE_VERSION" \
+        \
+        --build-arg BABEL_COMMIT="$BABEL_COMMIT" \
+        --build-arg CLDR_COMMIT="$CLDR_COMMIT" \
+        --build-arg BABEL_COMMIT="$BABEL_COMMIT" \
+        --build-arg CLDR_COMMIT="$CLDR_COMMIT" \
+        --build-arg CIRRUSSEARCH_COMMIT="$CIRRUSSEARCH_COMMIT" \
+        --build-arg CONFIRMEDIT_COMMIT="$CONFIRMEDIT_COMMIT" \
+        --build-arg ELASTICA_COMMIT="$ELASTICA_COMMIT" \
+        --build-arg ENTITYSCHEMA_COMMIT="$ENTITYSCHEMA_COMMIT" \
+        --build-arg NUKE_COMMIT="$NUKE_COMMIT" \
+        --build-arg OAUTH_COMMIT="$OAUTH_COMMIT" \
+        --build-arg SCRIBUNTO_COMMIT="$SCRIBUNTO_COMMIT" \
+        --build-arg SYNTAXHIGHLIGHT_GESHI_COMMIT="$SYNTAXHIGHLIGHT_GESHI_COMMIT" \
+        --build-arg UNIVERSALLANGUAGESELECTOR_COMMIT="$UNIVERSALLANGUAGESELECTOR_COMMIT" \
+        --build-arg VISUALEDITOR_COMMIT="$VISUALEDITOR_COMMIT" \
+        --build-arg WIKIBASECIRRUSSEARCH_COMMIT="$WIKIBASECIRRUSSEARCH_COMMIT" \
+        --build-arg WIKIBASEMANIFEST_COMMIT="$WIKIBASEMANIFEST_COMMIT" \
+        --build-arg WIKIBASEEDTF_COMMIT="$WIKIBASEEDTF_COMMIT" \
+        --build-arg WIKIBASELOCALMEDIA_COMMIT="$WIKIBASELOCALMEDIA_COMMIT" \
+        \
+        ./Docker/build/WikibaseBundle -t wikibase-bundle:${WMDE_RELEASE_VERSION}
 
-		docker save wikibase-bundle:${WMDE_RELEASE_VERSION} | gzip -"$GZIP_COMPRESSION_RATE" > artifacts/wikibase-bundle.docker.tar.gz
+    docker save wikibase-bundle:${WMDE_RELEASE_VERSION} | gzip -"$GZIP_COMPRESSION_RATE" > artifacts/wikibase-bundle.docker.tar.gz
 }
 
+function build_elasticseach {
+    echo "implement me"
+}
+
+function build_wdqs {
+    echo "implement me"
+}
+
+function build_wdqs-frontend {
+    echo "implement me"
+}
+
+function build_wdqs-proxy {
+    echo "implement me"
+}
+
+function build_quickstatements {
+    echo "implement me"
+}
 
 for arg in "$@"; do
     case $arg in
-        wikibase)
-						build_wikibase
-            ;;
-        -b|--optionB)
-						build_wikibase
-            ;;
-        *)
-            echo "Unknown option: $arg"
-						exit 2 # code appropriate?
-            ;;
-    esac
+        wikibase) build_wikibase ;;
+        elasticsearch) build_elasticseach ;;
+        wdqs) build_wdqs ;;
+        wdqs-frontend) build_wdqs-frontend ;;
+        wdqs-proxy) build_wdqs-proxy ;;
+    quickstatements) build_quickstatements ;; all)
+        build_wikibase
+        build_elasticseach
+        build_wdqs
+        build_wdqs-frontend
+        build_wdqs-proxy
+        build_quickstatements
+        ;;
+    *)
+        echo "Unknown option: $arg"
+        exit 2 # code appropriate?
+        ;;
+esac
 done
