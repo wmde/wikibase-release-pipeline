a2e0fda10db37af1d42757b75f6a9a1b47e45dc1
Ricki <141253288+RickiJay-WMDE@users.noreply.github.com>
2023-12-14 15:42:07 +0100

    T350879: Item Properties (#533)
    * Response Type
    
    * API test minor rework
    
    * Property
    
    * Unpaused
    
    * Redundant
    
    * Unused
    
    * Lines
    
    * One
    
    * Two
    
    * Three
    
    * Result: One + Three
    
    * Create
    
    * Passing
    
    * Job After
    
    * Pause Before Save I
    
    * Focus
    
    * Rename
    
    * Type Reorg
    
    * Typo
    
    * Import Clean

--- test/specs/repo_client/item.ts
+++ test/specs/repo_client/item.ts
@@ -3,7 +3,7 @@ import { stringify } from 'querystring';
 import LoginPage from 'wdio-mediawiki/LoginPage.js';
 import { getTestString } from 'wdio-mediawiki/Util.js';
 import WikibaseApi from 'wdio-wikibase/wikibase.api.js';
-import ExternalChange from '../../helpers/types/external-change.js';
+import ExternalChange from '../../types/external-change.js';
 
 const itemLabel = getTestString( 'The Item' );
 
