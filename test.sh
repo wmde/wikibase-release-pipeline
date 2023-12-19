b6168dcf8d6612bd74ef3b388e53335feab18783
Loren Johnson <lorenjohnson@gmail.com>
2023-12-19 13:00:40 +0100

    Adds DEBUG option to test.sh to see all Docker noise

--- test.sh
+++ test.sh
@@ -8,14 +8,31 @@ fi
 
 cd test
 
+# ./test.sh --shell
+# Updates NPM dependencies on test-runner
+if [ "$1" = "--npmInstall" ]; then
+	DEBUG=true
+	NPM_INSTALL=true
+fi
+
+# ./test.sh <suiteName> --setup
+# Starts the test environment and leaves it running, but does not run any specs
 if [ "$2" = "--setup" ]; then
 	SETUP=true
 fi
 
-TEST_RUNNER_COMPOSE="docker compose -f docker-compose.yml --env-file ./test-runner.env --env-file ../local.env --progress quiet"
+TEST_RUNNER_COMPOSE="docker compose -f docker-compose.yml --env-file ./test-runner.env --env-file ../local.env"
+
+optional_debug_output() {
+	if [ -n "$DEBUG" ]; then
+		"$@"
+	else
+		"$@" > /dev/null 2>&1
+	fi
+}
 
 function stop_test_runner {
-	$TEST_RUNNER_COMPOSE down > /dev/null 2>&1
+	optional_debug_output $TEST_RUNNER_COMPOSE down
 }
 
 if ! [ "$SETUP" ]; then
@@ -24,5 +41,12 @@ fi
 
 stop_test_runner
 
-$TEST_RUNNER_COMPOSE up -d --build > /dev/null 2>&1
-$TEST_RUNNER_COMPOSE run --rm test-runner -c "npx ts-node cli.js ${*:1}"
+optional_debug_output $TEST_RUNNER_COMPOSE up -d --build
+
+if [ "$NPM_INSTALL" ]; then
+	$TEST_RUNNER_COMPOSE run --rm test-runner -c "npm install"
+else
+	$TEST_RUNNER_COMPOSE run --rm test-runner -c "npx ts-node cli.js ${*:1}"
+fi
+
+
