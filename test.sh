aecda16f8e857f7d30c2160004b14cf6e8d75018
Loren Johnson <lorenjohnson@gmail.com>
2023-12-20 12:06:44 +0100

    Fix and suppress extraneous warnings from Docker and npm in test runs
    - Explicitly add the Docker network wikibase-suite-test to be shared  by test-runner and test-services
    - Sets default logging for npm in test-runner to error
    - Adds DEBUG option to test.sh to allow Docker feedback in cases where something is breaking

--- test.sh
+++ test.sh
@@ -8,14 +8,29 @@ fi
 
 cd test
 
+# ./test.sh <suiteName> --setup
+# Starts the test environment and leaves it running, but does not run any specs
 if [ "$2" = "--setup" ]; then
 	SETUP=true
 fi
 
+# Explicitly adds the Docker network wikibase-suite-test to shared by both
+# test-runner and test-services
+docker network create wikibase-suite-test > /dev/null 2>&1 || true
+
 TEST_RUNNER_COMPOSE="docker compose -f docker-compose.yml --env-file ./test-runner.env --env-file ../local.env --progress quiet"
 
+# Run as DEBUG=true ./test.sh <suiteName> to see logging of the Docker run
+optional_debug_output() {
+	if [ -n "$DEBUG" ]; then
+		"$@"
+	else
+		"$@" > /dev/null 2>&1
+	fi
+}
+
 function stop_test_runner {
-	$TEST_RUNNER_COMPOSE down > /dev/null 2>&1
+	optional_debug_output "$TEST_RUNNER_COMPOSE" down
 }
 
 if ! [ "$SETUP" ]; then
@@ -24,5 +39,6 @@ fi
 
 stop_test_runner
 
-$TEST_RUNNER_COMPOSE up -d --build > /dev/null 2>&1
+optional_debug_output "$TEST_RUNNER_COMPOSE" up -d --build
+
 $TEST_RUNNER_COMPOSE run --rm test-runner -c "npx ts-node cli.ts ${*:1}"
