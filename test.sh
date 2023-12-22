69b23f53d50cc58e643cb84f65e6c2b291428e7e
Loren Johnson <lorenjohnson@gmail.com>
2023-12-22 02:33:20 +0100

    T350782 / #542 wrap-up: Fixes Docker Image loading, standarizes docker usage in scripts, updates test docs (#553)
    - Correctly uses new wikibase/* namespace when checking for already-loaded Docker Images in test
    - Shares test-runner setup stuff in common with the lint.sh and test.sh scripts in "test/scripts/test_runner_setup.sh"
    - Adds a DEBUG option for "test.sh" which will passthrough Docker build and setup stuff in the case of mysterious issues when running the script
    - Adds a "--command", "-c" option for "test.sh" which will allow commands to be ran on test-runner
    - "lint.sh" now also builds test-runner like "test.sh" did
    - Explicitly creates the Docker network used by test-runner and test-services when it doesn't already exist
    - Test documentation update

--- test.sh
+++ test.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
-
 set -e
+source ./test/scripts/test_runner_setup.sh
 
-if ! [[ -f "local.env" ]]; then
-	touch local.env
+if ! [ "$2" = "--setup" ]; then
+	trap down_test_runner EXIT
 fi
 
-cd test
+down_test_runner
 
-if [ "$2" = "--setup" ]; then
-	SETUP=true
-fi
-
-TEST_RUNNER_COMPOSE="docker compose -f docker-compose.yml --env-file ./test-runner.env --env-file ../local.env --progress quiet"
-
-function stop_test_runner {
-	$TEST_RUNNER_COMPOSE down > /dev/null 2>&1
-}
-
-if ! [ "$SETUP" ]; then
-	trap stop_test_runner EXIT
-fi
-
-stop_test_runner
-
-$TEST_RUNNER_COMPOSE up -d --build > /dev/null 2>&1
-$TEST_RUNNER_COMPOSE run --rm test-runner -c "npx ts-node cli.ts ${*:1}"
+$TEST_COMPOSE up -d
+$RUN_TEST_RUNNER_CMD "npx ts-node cli.ts ${*:1}"
