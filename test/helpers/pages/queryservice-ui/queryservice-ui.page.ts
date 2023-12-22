c84dab4ac5b217aab5dac5cbdb08feca96cc7fe4
Loren Johnson <lorenjohnson@gmail.com>
2023-12-22 02:58:54 +0100

    Fixes it

--- test/helpers/pages/queryservice-ui/queryservice-ui.page.ts
+++ test/helpers/pages/queryservice-ui/queryservice-ui.page.ts
@@ -18,7 +18,7 @@ class QueryServiceUI extends SubmittablePage {
 		if ( prefixes ) {
 			query = prefixes.join( '\n' ) + '\n' + query;
 		}
-		return browser.url( `http://${testEnv.vars.WDQS_FRONTEND_URL}${'/#' + encodeURI( query )}` );
+		return browser.url( `${testEnv.vars.WDQS_FRONTEND_URL}${'/#' + encodeURI( query )}` );
 	}
 
 	public async resultIncludes( prop: string, value?: string ): Promise<boolean> {
