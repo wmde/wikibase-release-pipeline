d0680ca60a6d0e585b1a8093bec005f2361697e1
Loren Johnson <lorenjohnson@gmail.com>
2024-01-08 17:50:59 +0100

    Tidy-up Property to be more self-consistent

--- test/specs/repo/property.ts
+++ test/specs/repo/property.ts
@@ -1,16 +1,15 @@
-import assert from 'assert';
 import WikibaseApi from 'wdio-wikibase/wikibase.api.js';
 import Property from '../../helpers/pages/entity/property.page.js';
 import {
 	wikibasePropertyItem,
 	wikibasePropertyString
 } from '../../helpers/wikibase-property-types.js';
-import { Claim, EntityData, Reference } from '../../types/entity-data.js';
+import { Claim, Reference } from '../../types/entity-data.js';
 import WikibasePropertyType from '../../types/wikibase-property-type.js';
 
 const dataTypes = [ wikibasePropertyItem, wikibasePropertyString ];
 
-const propertyIdSelector = ( id: string ): string => `=${id} (${id})`; // =P1 (P1)
+const propertyIdSelector = ( id: string ): ChainablePromiseElement => $( `=${id} (${id})` ); // =P1 (P1)
 
 describe( 'Property', function () {
 	// eslint-disable-next-line mocha/no-setup-in-describe
@@ -36,15 +35,13 @@ describe( 'Property', function () {
 				await Property.addStatementLink.click();
 				// fill out property id for statement
 				await browser.keys( stringPropertyId.split( '' ) );
-				await $( propertyIdSelector( stringPropertyId ) ).click();
+				await propertyIdSelector( stringPropertyId ).click();
 				await browser.keys( 'STATEMENT'.split( '' ) );
-				// wait for save button to re-enable
 				await Property.saveStatementLink.click();
 			} );
 
 			it( 'Should be able to see added statement', async () => {
 				this.retries( 4 );
-
 				await expect( $( 'div=STATEMENT' ) ).toExist();
 				await expect( $( `aria/Property:${stringPropertyId}` ) ).toHaveText( stringPropertyId );
 			} );
@@ -54,32 +51,27 @@ describe( 'Property', function () {
 				// fill out property id for reference
 				await $( '.ui-entityselector-input' ).isFocused();
 				await browser.keys( stringPropertyId.split( '' ) );
-				await $( propertyIdSelector( stringPropertyId ) ).click();
+				await propertyIdSelector( stringPropertyId ).click();
 				await browser.keys( 'REFERENCE'.split( '' ) );
 				await Property.saveStatementLink.click();
 			} );
 
 			it( 'Should be able to see added reference', async function () {
 				this.retries( 4 );
-
 				await $( '=1 reference' ).click();
 				await expect( $( 'div=REFERENCE' ) ).toExist();
 			} );
 
 			it( 'Should contain statement and reference in EntityData', async function () {
 				this.retries( 4 );
-
 				const response = await browser.makeRequest(
 					`${testEnv.vars.WIKIBASE_URL}/wiki/Special:EntityData/${propertyId}.json`
 				);
-				const body: EntityData = response.data;
-				const claim: Claim =
-					body.entities[ propertyId ].claims[ stringPropertyId ][ 0 ];
-				const reference: Reference =
-					claim.references[ 0 ].snaks[ stringPropertyId ][ 0 ];
-
-				assert.strictEqual( claim.mainsnak.datavalue.value, 'STATEMENT' );
-				assert.strictEqual( reference.datavalue.value, 'REFERENCE' );
+				const claim: Claim = response.data.entities[ propertyId ]
+					.claims[ stringPropertyId ][ 0 ];
+				const reference: Reference = claim.references[ 0 ].snaks[ stringPropertyId ][ 0 ];
+				await expect( claim.mainsnak.datavalue.value ).toEqual( 'STATEMENT' );
+				await expect( reference.datavalue.value ).toEqual( 'REFERENCE' );
 			} );
 
 			it( 'Should show changes in "View history" tab', async () => {
