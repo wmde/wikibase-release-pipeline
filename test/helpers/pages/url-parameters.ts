90db1540323f1c95a195007e26ab186694ba3011
Ricki Jay <ricki.jay@wikimedia.de>
2024-01-27 00:46:58 +0100

    JSDocs II

--- test/helpers/pages/url-parameters.ts
+++ test/helpers/pages/url-parameters.ts
@@ -1,3 +1,7 @@
+/**
+ * @param {Object} parameters
+ * @return {string} `?param1=${param1}&param2=${param2}`
+ */
 function urlParameters( parameters: object ): string {
 	const output: string[] = Object.keys( parameters )
 		.filter( ( key: string ) => parameters[ key ] !== null )
