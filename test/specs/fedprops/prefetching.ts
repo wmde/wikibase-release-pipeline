90378c448447ce2ac458a6eb205a7b2ad708f993
Ricki Jay <ricki.jay@wikimedia.de>
2024-02-06 19:41:22 +0100

    Change Limit and Time

--- test/specs/fedprops/prefetching.ts
+++ test/specs/fedprops/prefetching.ts
@@ -87,16 +87,58 @@ describe( 'Property Prefetching', function () {
 	} );
 
 	it( 'Should be able to change limit', async () => {
+		await browser.url(
+			`${testEnv.vars.WIKIBASE_URL}/wiki/Special:RecentChanges?limit=50&days=7&urlversion=2&enhanced=0`
+		);
+		expect(
+			(
+				await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).getText()
+			).includes( '50 changes' )
+		);
 		await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).click();
-
-		const setChangeNumber = 100;
 		await $( 'div.mw-rcfilters-ui-changesLimitPopupWidget' )
-			.$( `span=${setChangeNumber}` )
+			.$( 'span=100' )
 			.click();
 		expect(
 			(
 				await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).getText()
-			).includes( `${setChangeNumber} changes` )
+			).includes( '100 changes' )
+		);
+	} );
+
+	it( 'Should be able to change time to 2 hours', async () => {
+		await browser.url(
+			`${testEnv.vars.WIKIBASE_URL}/wiki/Special:RecentChanges?limit=50&days=7&urlversion=2&enhanced=0`
+		);
+		expect(
+			(
+				await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).getText()
+			).includes( '7 days' )
+		);
+		await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).click();
+		await $( 'div.mw-rcfilters-ui-datePopupWidget-hours' ).$( 'span=2' ).click();
+		expect(
+			(
+				await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).getText()
+			).includes( '2 hours' )
+		);
+	} );
+
+	it( 'Should be able to change time to 3 days', async () => {
+		await browser.url(
+			`${testEnv.vars.WIKIBASE_URL}/wiki/Special:RecentChanges?limit=50&days=7&urlversion=2&enhanced=0`
+		);
+		expect(
+			(
+				await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).getText()
+			).includes( '7 days' )
+		);
+		await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).click();
+		await $( 'div.mw-rcfilters-ui-datePopupWidget-days' ).$( 'span=3' ).click();
+		expect(
+			(
+				await $( 'div.mw-rcfilters-ui-changesLimitAndDateButtonWidget' ).getText()
+			).includes( '3 days' )
 		);
 	} );
 } );
