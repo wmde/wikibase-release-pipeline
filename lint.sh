c54b94b262156cfc67191a9614802009e0308ded
Ricki Jay <ricki.jay@wikimedia.de>
2024-03-12 21:40:14 +0100

    Multiple

--- lint.sh
+++ lint.sh
@@ -14,21 +14,59 @@ done
 if $SHOULD_FIX
 then
   echo "Fixing Linting and Formatting Issues"
-  NPM_LINT_COMMAND="npm run lint:fix --silent"
+  # NPM_LINT_COMMAND="npm run lint:fix --silent"
+  ESLINT_FLAGS="--fix"
   NEWLINE_FLAGS="--fix"
   BLACK_FLAGS=""
 else
-  NPM_LINT_COMMAND="npm run lint --silent"
+  # NPM_LINT_COMMAND="npm run lint --silent"
+  ESLINT_FLAGS=""
   NEWLINE_FLAGS=""
   BLACK_FLAGS="--check"
 fi
 
-# ℹ️ Linting Javascript test/**/*.ts
-$TEST_COMPOSE run --rm --build \
-  -v "$(pwd)/.github":/usr/src/test/github \
-  -v "$(pwd)/docs":/usr/src/test/docs \
-  -v "$(pwd)/example":/usr/src/test/example \
-  test-runner -c "$NPM_LINT_COMMAND"
+# # ℹ️ Linting Javascript test/**/*.ts
+# $TEST_COMPOSE run --rm --build \
+#   -v "$(pwd)/.github":/usr/src/test/github \
+#   -v "$(pwd)/docs":/usr/src/test/docs \
+#   -v "$(pwd)/example":/usr/src/test/example \
+#   test-runner -c "$NPM_LINT_COMMAND"
+
+# ℹ️ Linting Javascript test/**/*.cjs,js,json,mjs,ts
+if $SHOULD_FIX
+then
+  $RUN_TEST_RUNNER_CMD "
+    npx prettier \"**/*.{cjs,js,json,mjs,ts}\" --write \
+      --log-level error
+  "
+fi
+$RUN_TEST_RUNNER_CMD "
+  npx eslint . --ext .cjs,.js,.json,.mjs,.ts $ESLINT_FLAGS
+"
+
+# ℹ️ Linting Markdown **/*.md
+if $SHOULD_FIX
+then
+  $RUN_TEST_RUNNER_CMD "
+    npx prettier \"../**/*.md\" --write \
+      --log-level error \
+      --config ./.prettierrc.json
+  "
+fi
+
+# ℹ️ Linting YML **/*.yml
+if $SHOULD_FIX
+then
+  $RUN_TEST_RUNNER_CMD "
+    npx prettier \"../**/*.yml\" --write \
+      --log-level error \
+      --config ./.prettierrc.json
+  "
+fi
+$RUN_TEST_RUNNER_CMD "
+  npx eslint .. --ext .yml $ESLINT_FLAGS \
+    --config ./.eslintrc.json
+"
 
 # ℹ️ Linting Shell Scripts (**/*.sh) - https://github.com/koalaman/shellcheck#from-your-terminal
 find . -type d -name node_modules -prune -false -o -name "*.sh" -print0 \
