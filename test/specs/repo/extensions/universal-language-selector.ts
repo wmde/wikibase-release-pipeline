6c7b75c901580c043fa417cb2331c4b446c17a57
Ricki Jay <ricki.jay@wikimedia.de>
2024-03-20 17:16:34 +0100

    Fix

--- test/specs/repo/extensions/universal-language-selector.ts
+++ test/specs/repo/extensions/universal-language-selector.ts
@@ -6,8 +6,16 @@ describe( 'UniversalLanguageSelector', function () {
 	it( 'Should be able to see the language selector menu', async function () {
 		await browser.url( testEnv.vars.WIKIBASE_URL );
 		await $( '#searchInput' ).click();
-		await $( '.imeselector' ).click();
 
-		await expect( $( '.imeselector-menu h3' ) ).toHaveText( 'English' );
+		// eslint-disable-next-line wdio/no-pause
+		await browser.pause( 1 * 1000 );
+
+		const selectors = await $$( 'div.imeselector' );
+		expect( selectors ).toHaveLength( 2 );
+		await selectors[ 1 ].click();
+
+		await expect(
+			$( 'div.imeselector-menu h3.ime-list-title' ).getHTML()
+		).resolves.toEqual( '<h3 class="ime-list-title autonym">English</h3>' );
 	} );
 } );
