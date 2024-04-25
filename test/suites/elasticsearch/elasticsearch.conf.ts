3556eec6fc4688c498e2e0c9276879c0e1587ae4
Ricki Jay <ricki.jay@wikimedia.de>
2024-04-25 13:08:43 +0200

    Lint

--- test/suites/elasticsearch/elasticsearch.conf.ts
+++ test/suites/elasticsearch/elasticsearch.conf.ts
@@ -1,10 +1,9 @@
-import { defaultTestSettings } from '../../setup/make-test-settings.js';
 import TestEnv from '../../setup/test-env.js';
 import wdioConfig from '../../setup/wdio.conf.js';
 
 export const testEnv = TestEnv.createWithDefaults( {
 	name: 'elasticsearch',
-	specs: [ 'specs/elasticsearch/*.ts' ],
+	specs: [ 'specs/elasticsearch/*.ts' ]
 } );
 
 export const config = wdioConfig( testEnv );
