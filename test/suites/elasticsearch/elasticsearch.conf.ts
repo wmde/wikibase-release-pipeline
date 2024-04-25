6f35be4fa529a4281c78ef0ece928a7b5e713a7b
Loren Johnson <lorenjohnson@gmail.com>
2024-04-25 12:06:41 +0200

    Use reverse proxy addresses vs host+port in tests. Propertly configure job runner. Remove redundent service definitions. docker-compose yml format consistency updates.

--- test/suites/elasticsearch/elasticsearch.conf.ts
+++ test/suites/elasticsearch/elasticsearch.conf.ts
@@ -5,10 +5,6 @@ import wdioConfig from '../../setup/wdio.conf.js';
 export const testEnv = TestEnv.createWithDefaults( {
 	name: 'elasticsearch',
 	specs: [ 'specs/elasticsearch/*.ts' ],
-	composeFiles: [
-		...defaultTestSettings.composeFiles,
-		'suites/elasticsearch/docker-compose.override.yml'
-	]
 } );
 
 export const config = wdioConfig( testEnv );
