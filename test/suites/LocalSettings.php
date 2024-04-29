27681150017b6431c2b0f78d0435029e3ec58867
Loren Johnson <lorenjohnson@gmail.com>
2024-04-29 14:37:18 +0200

    Remove repeated config in test. Make sure test LocalSettings.php override is ran at end

--- test/suites/LocalSettings.php
+++ test/suites/LocalSettings.php
@@ -4,11 +4,3 @@ $wgShowExceptionDetails = true;
 $wgShowSQLErrors = true;
 $wgDebugDumpSql  = false;
 $wgShowDBErrorBacktrace = false;
-$wgDebugLogFile = '/var/log/mediawiki/mw.debug.log';
-
-$wgDebugLogGroups = array(
-	'resourceloader' => '/var/log/mediawiki/mw.resourceloader.log',
-	'exception' => '/var/log/mediawiki/mw.exception.log',
-	'error' => '/var/log/mediawiki/mw.error.log',
-	'fatal' => '/var/log/mediawiki/mw.fatal.log',
-);
