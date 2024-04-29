d7127d7493cb7f4f4fb67b1d3c40feb2cda1a388
Loren Johnson <lorenjohnson@gmail.com>
2024-04-29 08:39:47 +0200

    Revert logging to stdout/stderr for now

--- build/Wikibase/LocalSettings.additions.php
+++ build/Wikibase/LocalSettings.additions.php
@@ -1,11 +1,15 @@
+# NOTE: This should already be the default, but wasn't in testing. Perhaps the official MediaWiki base we
+# base WBS Wikibase off of changes this.
+$wgScriptPath = "/w";
+$wgArticlePath = "/wiki/$1";
+
 # Logs
-# Save these logs inside the container
-# TODO: do we need those? Can we not write to the container for logging?
+# TODO: Explore simply logging to stdout/stderr so these appear in Docker logs
 $wgDebugLogGroups = array(
-	'resourceloader' => '/dev/stdout',
-	'exception' => '/dev/stderr',
-	'error' => '/dev/stderr',
-	'fatal' => '/dev/stderr',
+	'resourceloader' => '/var/log/mediawiki/resourceloader.log',
+	'exception' => '/var/log/mediawiki/exception.log',
+	'error' => '/var/log/mediawiki/error.log',
+	'fatal' => '/var/log/mediawiki/fatal.log',
 );
 
 # Number of jobs run on request, 0 with job runner, above 0 without job runner
