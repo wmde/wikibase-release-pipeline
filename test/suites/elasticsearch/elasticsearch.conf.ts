1a8c6f47c34357caf5db5e971211a4200dbd8487
Loren Johnson <lorenjohnson@gmail.com>
2024-04-29 19:28:21 +0200

    Speed-up test run times by disabling elasticsearch for Wikibase service to avoid long indexing time

--- test/suites/elasticsearch/elasticsearch.conf.ts
+++ test/suites/elasticsearch/elasticsearch.conf.ts
@@ -1,9 +1,14 @@
+import { defaultTestSettings } from '../../setup/make-test-settings.js';
 import TestEnv from '../../setup/test-env.js';
 import wdioConfig from '../../setup/wdio.conf.js';
 
 export const testEnv = TestEnv.createWithDefaults( {
 	name: 'elasticsearch',
-	specs: [ 'specs/elasticsearch/*.ts' ]
+	specs: [ 'specs/elasticsearch/*.ts' ],
+	composeFiles: [
+		...defaultTestSettings.composeFiles,
+		'suites/elasticsearch/docker-compose.override.yml'
+	]
 } );
 
 export const config = wdioConfig( testEnv );
