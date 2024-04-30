70488f33f9b376d5b9119d1a0ad00f46b4e05d4e
Loren Johnson <lorenjohnson@gmail.com>
2024-04-30 18:01:43 +0200

    Fix issue

--- build/Wikibase/LocalSettings.wikibase.php
+++ build/Wikibase/LocalSettings.wikibase.php
@@ -15,14 +15,25 @@ $wgDebugLogFile = '/var/log/mediawiki/mw.debug.log';
 $wgScriptPath = "/w";
 $wgArticlePath = "/wiki/$1";
 
-$wgServer = $_ENV['MW_WG_SERVER'];
-$wgSiteName = $_ENV['MW_WG_SITENAME'];
-$wgLanguageCode = $_ENV['MW_WG_LANGUAGE_CODE'];
-$wgEnableUploads = $_ENV['MW_WG_ENABLE_UPLOADS'];
-$wgJobRunRate = $_ENV['MW_WG_JOB_RUN_RATE'];
+if (isset($_ENV['MW_WG_SERVER'])) {
+	$wgServer = $_ENV['MW_WG_SERVER'];
+}
+if (isset($_ENV['MW_WG_SITENAME'])) {
+	$wgSiteName = $_ENV['MW_WG_SITENAME'];
+}
+if (isset($_ENV['MW_WG_LANGUAGE_CODE'])) {
+	$wgLanguageCode = $_ENV['MW_WG_LANGUAGE_CODE'];
+}
+if (isset($_ENV['MW_WG_ENABLE_UPLOADS'])) {
+	$wgEnableUploads = $_ENV['MW_WG_ENABLE_UPLOADS'];
+}
+if (isset($_ENV['MW_WG_ENABLE_UPLOADS'])) {
+	$wgJobRunRate = $_ENV['MW_WG_JOB_RUN_RATE'];
+}
 
-# Pingback
-$wgWBRepoSettings['wikibasePingback'] = $_ENV['WIKIBASE_PINGBACK'];
+if (isset($_ENV['WIKIBASE_PINGBACK'])) {
+	$wgWBRepoSettings['wikibasePingback'] = $_ENV['WIKIBASE_PINGBACK'];
+}
 
 # Wikibase Repository
 wfLoadExtension( 'WikibaseRepository', "$IP/extensions/Wikibase/extension-repo.json" );
