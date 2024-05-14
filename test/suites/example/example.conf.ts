e720a9b5e89e121e618d4338e19608ce4de9b712
Loren Johnson <lorenjohnson@gmail.com>
2024-05-14 15:36:50 +0200

    Add DEPRECATED note to Example test suite

--- test/suites/example/example.conf.ts
+++ test/suites/example/example.conf.ts
@@ -2,6 +2,28 @@ import { defaultTestSettings } from '../../setup/make-test-settings.js';
 import TestEnv from '../../setup/test-env.js';
 import wdioConfig from '../../setup/wdio.conf.js';
 
+// TODO: The specs and setup in this suite are deprecated
+// now that the entire test suite runs off the Example
+// configuration.
+//
+// The user stories which reflect our current Example design,
+// among other things, are likely to cover the now deprecated
+// Upgrade test suite.
+// 
+// Here are some proposed stories to cover:
+//
+// 1. User can setup and run all WBS services
+// 2. User can stop and restart WBS services and all services
+//    come up (no data change)
+// 3. User can stop and restart WBS services and all services
+//    come up (with data change). Checks if data remains.
+// 4. A Wikibase and/or other services Docker Image upgrades happen
+//    automatically on restart of services
+// 5. Can change setup variables but instance is unchanged if a
+//    /config/LocalSettings.php exists. 
+// 6. Wikibase instance is re-setup if /config/LocalSettings.php
+//    is not provided.
+
 export const testEnv = TestEnv.createWithDefaults( {
 	name: 'example',
 	specs: [
