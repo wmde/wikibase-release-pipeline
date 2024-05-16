f1cc146a2a281290e3bf34a6a4deef0d8d8f2875
Loren Johnson <lorenjohnson@gmail.com>
2024-05-16 10:16:50 +0200

    Enable file uploads by default

--- build/Wikibase/LocalSettings.wbs.php
+++ build/Wikibase/LocalSettings.wbs.php
@@ -3,6 +3,9 @@
 // JobRunner instance is assumed by default, so jobs on request is disabled
 $wgJobRunRate = 0;
 
+// File Uploads enabled by default
+$wgEnableUploads = true;
+
 # Logs
 # TODO: Explore simply logging to stdout/stderr so these appear in Docker logs
 $wgDebugLogGroups = array(
