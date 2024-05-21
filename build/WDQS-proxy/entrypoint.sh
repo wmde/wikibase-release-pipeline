21cf347956c35a1f1514ddc76c2f57ca17c3cd42
Loren Johnson <lorenjohnson@gmail.com>
2024-05-21 19:10:32 +0200

    T365141, T364956: Change default to HTTPS in example and uploads enabled for Wikibase. Set max file upload size in parity with MediaWiki, change to Traefik from nginx-proxy. (#675)

--- build/WDQS-proxy/entrypoint.sh
+++ build/WDQS-proxy/entrypoint.sh
@@ -3,8 +3,8 @@
 
 # Test if required environment variables have been set
 if [ -z "$PROXY_PASS_HOST" ]; then
-echo "PROXY_PASS_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
-exit 1;
+  echo "PROXY_PASS_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+  exit 1;
 fi
 
 set -eu
