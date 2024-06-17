9f661a9785a0e4fc3e78249e85b452808043ab6a
adeeri <adee.ritman@wikimedia.de>
2024-06-17 16:26:53 +0200

    Plus sign not allowed

--- versions.inc.sh
+++ versions.inc.sh
@@ -44,11 +44,11 @@ function version_tags() {
 
     # Extra tags
     if [[ "$image_name" == "wikibase" ]]; then
-        tags+=("${WIKIBASE_VERSION}+mw-${MEDIAWIKI_VERSION}")
+        tags+=("${WIKIBASE_VERSION}-mw${MEDIAWIKI_VERSION}")
     elif [[ "$image_name" == "elasticsearch" ]]; then
-        tags+=("${ELASTICSEARCH_WBS_VERSION}+${ELASTICSEARCH_VERSION}")
+        tags+=("${ELASTICSEARCH_WBS_VERSION}-${ELASTICSEARCH_VERSION}")
     elif [[ "$image_name" == "wdqs" ]]; then
-        tags+=("${WDQS_WBS_VERSION}+${WDQS_VERSION}")
+        tags+=("${WDQS_WBS_VERSION}-${WDQS_VERSION}")
     fi
 
     printf "%s\n" "${tags[@]}"
