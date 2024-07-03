27d328172471eee8774d5a7aadddf8ab07e25697
Loren Johnson <lorenjohnson@gmail.com>
2024-07-03 11:54:36 +0200

    Linting

--- test/specs/repo/extensions/universal-language-selector.ts
+++ test/specs/repo/extensions/universal-language-selector.ts
@@ -1,4 +1,4 @@
-import { parseSemVer } from 'semver-parser'
+import { parseSemVer } from 'semver-parser';
 import page from '../../../helpers/pages/page.js';
 
 describe( 'UniversalLanguageSelector', function () {
@@ -10,21 +10,14 @@ describe( 'UniversalLanguageSelector', function () {
 		await page.open( '' );
 
 		// MediaWiki 1.39 default skin sets up language selector differently than subsequent versions,
-		// this exception can be removed once MW 1.39 support is deprecated.
-		if (parseSemVer(testEnv.vars.MEDIAWIKI_VERSION).minor == 39) {
-			await $( '#searchInput' ).click();
-			await $( '.imeselector' ).click();
-	
-			await expect( $( '.imeselector-menu h3' ) ).toHaveText( 'English' );
-		} else {
+		// this exception can be removed once MW 1.39 is no longer supported.
+		if ( parseSemVer( testEnv.vars.MEDIAWIKI_VERSION ).minor > 39 ) {
 			await $( '#searchform input' ).click();
-
 			// work around lang selector not showing up the first time
 			// blur the search bar
 			await $( '.page-Main_Page' ).click();
 			// focus search bar again, lang selector should be there now
 			await $( '#searchform input' ).click();
-
 			await $$( '.imeselector' )
 				.filter( async ( selector ) => selector.isClickable() )[ 0 ]
 				.click();
@@ -35,6 +28,11 @@ describe( 'UniversalLanguageSelector', function () {
 			await expect(
 				$( 'div.imeselector-menu h3.ime-list-title' ).getHTML()
 			).resolves.toMatch( /English/ );
+		} else {
+			await $( '#searchInput' ).click();
+			await $( '.imeselector' ).click();
+
+			await expect( $( '.imeselector-menu h3' ) ).toHaveText( 'English' );
 		}
 	} );
 } );
