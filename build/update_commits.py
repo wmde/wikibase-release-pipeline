a13e64b1bad9e05634009c718e4f458a32a0255e
Loren Johnson <lorenjohnson@gmail.com>
2024-07-24 00:51:00 +0200

    Move update-commits task into nx

--- build/update_commits.py
+++ build/update_commits.py
@@ -1,4 +1,4 @@
-import glob, json, re, requests
+import os, json, re, requests
 from bs4 import BeautifulSoup
 
 def get_commit(variable: str, url: str, parse_commit: callable, previous_commit: str):
@@ -52,61 +52,67 @@ def parse_bitbucket_commit(response: requests.Response) -> str:
     return data["values"][0]["hash"]
 
 
-def run():
-    # Get all build.env files in subdirectories of the current directory
-    for build_env_path in glob.glob("**/**/build.env"):
-        print(f"Processing {build_env_path}")
+def run(file_path):
+    if not os.path.exists(file_path):
+        print(f"File {file_path} does not exist.")
+        return
 
-        with open(build_env_path, "r") as variable_file:
-            variable_contents = variable_file.read()
+    print(f"Processing {file_path}")
 
-        mediawiki_match = re.search(r"MEDIAWIKI_VERSION=(\d+)\.(\d+)", variable_contents)
-        if mediawiki_match:
-            rel = f"REL{mediawiki_match.group(1)}_{mediawiki_match.group(2)}"
-            print(f"Mediawiki Version:\t{mediawiki_match.group(1)}.{mediawiki_match.group(2)}")
-            variable_contents = re.sub(r"\bREL\d+_\d+", rel, variable_contents)
+    with open(file_path, "r") as variable_file:
+        variable_contents = variable_file.read()
 
-        for gerrit_commit in re.findall(gerrit_pattern, variable_contents):
-            if commit := get_commit(
-                gerrit_commit[1],
-                gerrit_commit[0],
-                parse_gerrit_commit,
-                gerrit_commit[2],
-            ):
-                variable_contents = re.sub(
-                    f"{gerrit_commit[1]}=[0-9a-f]+",
-                    f"{gerrit_commit[1]}={commit}",
-                    variable_contents,
-                )
+    mediawiki_match = re.search(r"MEDIAWIKI_VERSION=(\d+)\.(\d+)", variable_contents)
+    if mediawiki_match:
+        rel = f"REL{mediawiki_match.group(1)}_{mediawiki_match.group(2)}"
+        print(f"Mediawiki Version:\t{mediawiki_match.group(1)}.{mediawiki_match.group(2)}")
+        variable_contents = re.sub(r"\bREL\d+_\d+", rel, variable_contents)
 
-        for github_commit in re.findall(github_pattern, variable_contents):
-            if commit := get_commit(
-                github_commit[2],
-                f"https://api.github.com/repos/{github_commit[1]}",
-                parse_github_commit,
-                github_commit[3],
-            ):
-                variable_contents = re.sub(
-                    f"{github_commit[2]}=[0-9a-f]+",
-                    f"{github_commit[2]}={commit}",
-                    variable_contents,
-                )
+    for gerrit_commit in re.findall(gerrit_pattern, variable_contents):
+        if commit := get_commit(
+            gerrit_commit[1],
+            gerrit_commit[0],
+            parse_gerrit_commit,
+            gerrit_commit[2],
+        ):
+            variable_contents = re.sub(
+                f"{gerrit_commit[1]}=[0-9a-f]+",
+                f"{gerrit_commit[1]}={commit}",
+                variable_contents,
+            )
 
-        for bitbucket_commit in re.findall(bitbucket_pattern, variable_contents):
-            if commit := get_commit(
-                bitbucket_commit[2],
-                f"https://bitbucket.org/!api/2.0/repositories/{bitbucket_commit[1]}",
-                parse_bitbucket_commit,
-                bitbucket_commit[3],
-            ):
-                variable_contents = re.sub(
-                    f"{bitbucket_commit[2]}=[0-9a-f]+",
-                    f"{bitbucket_commit[2]}={commit}",
-                    variable_contents,
-                )
+    for github_commit in re.findall(github_pattern, variable_contents):
+        if commit := get_commit(
+            github_commit[2],
+            f"https://api.github.com/repos/{github_commit[1]}",
+            parse_github_commit,
+            github_commit[3],
+        ):
+            variable_contents = re.sub(
+                f"{github_commit[2]}=[0-9a-f]+",
+                f"{github_commit[2]}={commit}",
+                variable_contents,
+            )
 
-        with open(build_env_path, "w") as variable_file:
-            variable_file.write(variable_contents)
+    for bitbucket_commit in re.findall(bitbucket_pattern, variable_contents):
+        if commit := get_commit(
+            bitbucket_commit[2],
+            f"https://bitbucket.org/!api/2.0/repositories/{bitbucket_commit[1]}",
+            parse_bitbucket_commit,
+            bitbucket_commit[3],
+        ):
+            variable_contents = re.sub(
+                f"{bitbucket_commit[2]}=[0-9a-f]+",
+                f"{bitbucket_commit[2]}={commit}",
+                variable_contents,
+            )
+
+    with open(file_path, "w") as variable_file:
+        variable_file.write(variable_contents)
 
 if __name__ == "__main__":
-    run()
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <path_to_env_file>")
+    else:
+        run(sys.argv[1])
