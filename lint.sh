94d709275a8ddd6cb1b5c12a6ba9ed68443e33d1
Loren Johnson <lorenjohnson@gmail.com>
2024-07-27 10:18:37 +0200

    Move linting to root as linting is about all the code in the repo

--- lint.sh
+++ lint.sh
@@ -3,21 +3,21 @@
 # ℹ️ Linting JS, YAML, Python scripts, and general whitespace (optionally fix)
 if [[ $1 == "--fix" || $1 == "-f" ]]; then
   echo "ℹ️ Fixing linting issues"
-  npm run lint:fix --workspace test
+  npm run lint:fix
   # TODO: We only have 1 python script, should we do away with the Python dependency
   # and use Typescript/Javascript utility scripts instead?
-  python3 -m black ../**/*.py
+  python3 -m black ./**/*.py
 else
-  npm run lint:js-and-yml --workspace test
-  npm run lint:whitespace --workspace test
-  python3 -m black ../**/*.py --check
+  npm run lint:js-and-yml
+  npm run lint:whitespace
+  python3 -m black ./**/*.py --check
 fi
 
 # ℹ️ Linting shell scripts (*.sh) with shellcheck
-find .. -type d \( -name node_modules -o -name .git \) -prune -o -type f -name "*.sh" -print0 | \
+find . -type d \( -name node_modules -o -name .git \) -prune -o -type f -name "*.sh" -print0 | \
   xargs -0 shellcheck
-shellcheck ../nx
+shellcheck ./nx
 
 # ℹ️ Linting Dockerfiles (**/Dockerfile) - https://github.com/hadolint/hadolint
-find .. -type d \( -name node_modules -o -name .git \) -prune -o -type f -name Dockerfile -print0 | \
+find . -type d \( -name node_modules -o -name .git \) -prune -o -type f -name Dockerfile -print0 | \
   xargs -0 hadolint --config .hadolint.yml
