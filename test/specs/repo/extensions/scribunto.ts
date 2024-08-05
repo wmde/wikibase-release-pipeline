31e74148e3436c69ef0f9d2def88ef4a17e02ba1
adeeri <adee.ritman@wikimedia.de>
2024-08-05 14:51:43 +0200

    Keep some tests

---
+++ test/specs/repo/extensions/scribunto.ts
@@ -0,0 +1,40 @@
+import { readFile } from 'fs/promises';
+import { utf8 } from '../../../helpers/read-file-encoding.js';
+
+describe( 'Scribunto', function () {
+	beforeEach( async function () {
+		await browser.skipIfExtensionNotPresent( this, 'Scribunto' );
+	} );
+
+	it( 'Should be able to execute lua module', async function () {
+		// eslint-disable-next-line security/detect-non-literal-fs-filename
+		const fileContents = await readFile(
+			new URL( 'bananas.lua', import.meta.url ),
+			utf8
+		);
+		await browser.editPage(
+			testEnv.vars.WIKIBASE_URL,
+			'Module:Bananas',
+			fileContents
+		);
+
+		const executionContent = await browser.editPage(
+			testEnv.vars.WIKIBASE_URL,
+			'LuaTest',
+			'{{#invoke:Bananas|hello}}'
+		);
+
+		// should come from executed lua script
+		expect( executionContent ).toMatch( 'Hello, world!' );
+	} );
+
+	it( 'Should be able to execute lua module within 0.05 seconds', async function () {
+		const cpuTime = await browser.getLuaCpuTime(
+			testEnv.vars.WIKIBASE_URL,
+			'LuaTest'
+		);
+
+		expect( cpuTime.value ).toBeLessThan( 0.05 );
+		expect( cpuTime.scale ).toEqual( 'seconds' );
+	} );
+} );
---
+++ test/specs/repo/extensions/visual-editor.ts
@@ -0,0 +1,32 @@
+import page from '../../../helpers/pages/page.js';
+
+describe( 'VisualEditor', function () {
+	it( 'Should be able to edit a page using the editor', async function () {
+		await page.open( '/wiki/TestVisualEditor?veaction=edit' );
+
+		// start editing
+		await $( '.oo-ui-messageDialog-actions' ).waitForDisplayed();
+		const messageDialogAEl = await $$( '.oo-ui-messageDialog-actions a' );
+		await messageDialogAEl[ 1 ].click();
+
+		// eslint-disable-next-line wdio/no-pause
+		await browser.pause( 5 * 1000 );
+
+		// disable notice popup and focus on editor
+		await $( '.oo-ui-tool-name-notices .oo-ui-tool-link' ).click();
+		await $( '.ve-ce-surface' ).click();
+
+		await browser.keys( [ 'T', 'E', 'S', 'T' ] );
+
+		// save changes
+		await $( 'a.ve-ui-toolbar-saveButton' ).click();
+
+		// fill-out for summary popup and submit
+		await $( '.oo-ui-inputWidget-input' );
+		await browser.keys( 'X' );
+
+		await $( '.oo-ui-processDialog-actions-primary a' ).click();
+
+		await expect( $( '.mw-parser-output' ) ).toHaveText( 'TEST' );
+	} );
+} );
