960ee8dcd2f0132d3ea776478a31bc234348c9b3
Loren Johnson <lorenjohnson@gmail.com>
2024-08-07 20:44:30 +0200

    Move back to node-native nx

--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,7 @@
 # Change to the directory where the script is located
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
+# Make sure the docker network exists
+docker network create wbs-dev > /dev/null 2>&1 || true
+
 NODE_NO_WARNINGS=1 node --require ts-node/register --loader=ts-node/esm cli.ts "$@"
