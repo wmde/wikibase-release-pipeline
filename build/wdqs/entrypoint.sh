39795c6884f901e3e1d5cf164b32924dff6834a5
Robert Timm <mail@rtti.de>
2024-11-13 12:56:58 +0000

    fix: keep wd: et al. prefixes for wikidata

--- build/wdqs/entrypoint.sh
+++ build/wdqs/entrypoint.sh
@@ -13,7 +13,24 @@ done
 
 set -eu
 
-export BLAZEGRAPH_OPTS="${BLAZEGRAPH_EXTRA_OPTS} -DwikibaseHost=${WIKIBASE_HOST} -DwikibaseConceptUri=${WIKIBASE_CONCEPT_URI}"
+# Options provided to WDQS (blazegraph) when running as query service instance
+#
+# Note: We MUST not provide -DwikibaseHost=${WIKIBASE_HOST} here, otherwise
+# WDQS would re-use the wd: et al. prefixes for the local wikibase instance.
+# This is unintended, especially in the context of federation. wd: prefixes
+# should remain in place for referencing wikidata. The local instance should
+# choose its own prefixes, as described here: 
+# https://www.mediawiki.org/wiki/Wikibase/Wikibase.cloud/First_steps#View_your_data_using_the_Query_Service
+# Some further thoughts on prefixes: https://phabricator.wikimedia.org/T335448
+#
+# In other words, WDQS does not know about the hostname of the wiki it gets
+# its data from. Is is solely the task of the updater to feed data from the
+# wiki into the WDQS instance.
+export BLAZEGRAPH_OPTS="${BLAZEGRAPH_EXTRA_OPTS}"
+
+# Options provided when running as wdqs-updater
+#
+# Here we provide -DwikibaseHost to reference the wiki to poll updates from.
 export UPDATER_OPTS="-DwikibaseHost=${WIKIBASE_HOST} -DwikibaseMaxDaysBack=${WIKIBASE_MAX_DAYS_BACK}"
 
 envsubst < /templates/mwservices.json > /wdqs/mwservices.json
