1646c7a84e47f81aed12002c4d7eac1037768d24
Robert Timm <mail@rtti.de>
2025-02-28 08:34:14 +0000

    fix: wdqs-frontend env var handling

--- build/wdqs-frontend/entrypoint.sh
+++ build/wdqs-frontend/entrypoint.sh
@@ -2,28 +2,27 @@
 # This file is provided by the wikibase/wdqs-frontend docker image.
 
 # Test if required environment variables have been set
-if [ -z "$WIKIBASE_HOST" ]; then
-	echo "WIKIBASE_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+if [ -z "$WIKIBASE_PUBLIC_URL" ]; then
+	echo "Environment variable WIKIBASE_PUBLIC_URL is required but isn't set.";
 	exit 1;
 fi
 
-if [ -z "$WDQS_HOST" ]; then
-	echo "WDQS_HOST is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+if [ -z "$WDQS_PUBLIC_URL" ]; then
+	echo "Environment variable WDQS_PUBLIC_URL is required but isn't set.";
 	exit 1;
 fi
 
 set -eu
+export DOLLAR='$'
 
-# Take config from user config dir if present
-if [ -e "/config/wdqs-frontend-config.json" ]; then
-    cp /config/wdqs-frontend-config.json /usr/share/nginx/html/custom-config.json
-
-# Otherwise, make our stock config visible to the user for customization
-else
-    cp /usr/share/nginx/html/custom-config.json /config/wdqs-frontend-config.json
+# Generate initial config from template and put it to /config
+if ! [ -e "/config/wdqs-frontend-config.json" ]; then
+    envsubst < /templates/wdqs-frontend-config.json.template > /config/wdqs-frontend-config.json
 fi
 
-export DOLLAR='$'
+# Use config from /config
+cp /config/wdqs-frontend-config.json /usr/share/nginx/html/custom-config.json
+
 envsubst < /templates/nginx-default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
