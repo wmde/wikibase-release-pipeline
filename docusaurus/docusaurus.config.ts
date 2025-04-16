ad814e6b63828dc68967c2e2db16f0c478b9bb1f
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-04-17 00:54:33 +0200

    WMDE

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -110,7 +110,7 @@ const config: Config = {
 				// 	]
 				// }
 			],
-			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+			copyright: `Copyright © ${new Date().getFullYear()} WMDE. Built with Docusaurus.`
 		},
 		prism: { theme: prismThemes.github, darkTheme: prismThemes.dracula }
 	} satisfies Preset.ThemeConfig
