d92a0e8a3bee9e49a3469e9d700176640ab54e45
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-04-16 23:06:57 +0200

    Config

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -5,9 +5,9 @@ import { themes as prismThemes } from 'prism-react-renderer';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-	title: 'My Site',
-	tagline: 'Dinosaurs are cool',
-	favicon: 'img/favicon.ico',
+	title: 'Wikibase Suite',
+	tagline: 'Because for some reason, you decided to host a Wikibase',
+	favicon: 'img/wikibase-symbol/wikibase_symbol_rgb.svg',
 
 	// Set the production url of your site here
 	url: 'https://your-docusaurus-site.example.com',
@@ -17,8 +17,8 @@ const config: Config = {
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'facebook', // Usually your GitHub org/user name.
-	projectName: 'docusaurus', // Usually your repo name.
+	organizationName: 'wmde', // Usually your GitHub org/user name.
+	projectName: 'wikibase-release-pipeline', // Usually your repo name.
 
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
@@ -39,18 +39,18 @@ const config: Config = {
 					// editUrl:
 					// 	'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
 				},
-				blog: {
-					showReadingTime: true,
-					feedOptions: { type: ['rss', 'atom'], xslt: true },
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					// editUrl:
-					// 	'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-					// Useful options to enforce blogging best practices
-					onInlineTags: 'warn',
-					onInlineAuthors: 'warn',
-					onUntruncatedBlogPosts: 'warn'
-				},
+				// blog: {
+				// 	showReadingTime: true,
+				// 	feedOptions: { type: ['rss', 'atom'], xslt: true },
+				// 	// Please change this to your repo.
+				// 	// Remove this to remove the "edit this page" links.
+				// 	// editUrl:
+				// 	// 	'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				// 	// Useful options to enforce blogging best practices
+				// 	onInlineTags: 'warn',
+				// 	onInlineAuthors: 'warn',
+				// 	onUntruncatedBlogPosts: 'warn'
+				// },
 				theme: { customCss: './src/css/custom.css' }
 			} satisfies Preset.Options
 		]
@@ -58,10 +58,10 @@ const config: Config = {
 
 	themeConfig: {
 		// Replace with your project's social card
-		image: 'img/docusaurus-social-card.jpg',
+		// image: 'img/docusaurus-social-card.jpg',
 		navbar: {
-			title: 'My Site',
-			logo: { alt: 'My Site Logo', src: 'img/logo.svg' },
+			title: 'Wikibase Suite',
+			logo: { alt: 'WBS', src: 'img/wikibase-symbol/wikibase_symbol_rgb.svg' },
 			items: [
 				{
 					type: 'docSidebar',
@@ -69,12 +69,13 @@ const config: Config = {
 					position: 'left',
 					label: 'Tutorial'
 				},
-				{ to: '/blog', label: 'Blog', position: 'left' },
+				// { to: '/blog', label: 'Blog', position: 'left' },
 				{
-					href: 'https://github.com/facebook/docusaurus',
+					href: 'https://github.com/wmde/wikibase-release-pipeline/',
 					label: 'GitHub',
 					position: 'right'
-				}
+				},
+				{ type: 'docsVersionDropdown' }
 			]
 		},
 		footer: {
@@ -83,28 +84,28 @@ const config: Config = {
 				{
 					title: 'Docs',
 					items: [{ label: 'Tutorial', to: '/docs/intro' }]
-				},
-				{
-					title: 'Community',
-					items: [
-						{
-							label: 'Stack Overflow',
-							href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-						},
-						{
-							label: 'Discord',
-							href: 'https://discordapp.com/invite/docusaurus'
-						},
-						{ label: 'X', href: 'https://x.com/docusaurus' }
-					]
-				},
-				{
-					title: 'More',
-					items: [
-						{ label: 'Blog', to: '/blog' },
-						{ label: 'GitHub', href: 'https://github.com/facebook/docusaurus' }
-					]
 				}
+				// {
+				// 	title: 'Community',
+				// 	items: [
+				// 		{
+				// 			label: 'Stack Overflow',
+				// 			href: 'https://stackoverflow.com/questions/tagged/docusaurus'
+				// 		},
+				// 		{
+				// 			label: 'Discord',
+				// 			href: 'https://discordapp.com/invite/docusaurus'
+				// 		},
+				// 		{ label: 'X', href: 'https://x.com/docusaurus' }
+				// 	]
+				// },
+				// {
+				// 	title: 'More',
+				// 	items: [
+				// 		{ label: 'Blog', to: '/blog' },
+				// 		{ label: 'GitHub', href: 'https://github.com/facebook/docusaurus' }
+				// 	]
+				// }
 			],
 			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
 		},
