e07f9ae5374217198ab50cd9bc14b8e2e93d44f1
ricki-jay-wmde <ricki.jay@wikimedia.de>
2025-04-16 23:10:18 +0200

    Home

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -1,6 +1,4 @@
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
@@ -8,7 +6,7 @@ import type { ReactNode } from "react";
 
 import styles from "./index.module.css";
 
-function HomepageHeader() {
+function HomepageHeader(): ReactNode {
 	const { siteConfig } = useDocusaurusContext();
 	return (
 		<header className={clsx("hero hero--primary", styles.heroBanner)}>
@@ -17,14 +15,6 @@ function HomepageHeader() {
 					{siteConfig.title}
 				</Heading>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
-				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--lg"
-						to="/docs/intro"
-					>
-						Docusaurus Tutorial - 5min ⏱️
-					</Link>
-				</div>
 			</div>
 		</header>
 	);
@@ -34,13 +24,11 @@ export default function Home(): ReactNode {
 	const { siteConfig } = useDocusaurusContext();
 	return (
 		<Layout
-			title={`Hello from ${siteConfig.title}`}
+			title={siteConfig.title}
 			description="Description will go into a meta tag in <head />"
 		>
 			<HomepageHeader />
-			<main>
-				<HomepageFeatures />
-			</main>
+			<main></main>
 		</Layout>
 	);
 }
