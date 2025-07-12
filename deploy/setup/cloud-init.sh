ec4b97747b1270a85a0cdebb97f308d8d6c7d7d8
Loren Johnson <lorenjohnson@gmail.com>
2025-07-12 05:40:58 +0200

    test

--- deploy/setup/cloud-init.sh
+++ deploy/setup/cloud-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 curl -fsSL https://raw.githubusercontent.com/wmde/wikibase-release-pipeline/refs/heads/cloud-config-test/deploy/setup/cloud-init.yml -o /tmp/cloud-init.yml
-sudo cloud-init single --file /tmp/cloud-init.yml --name runcmd --frequency always
+# sudo cloud-init single --file /tmp/cloud-init.yml --name runcmd --frequency always
+sudo cloud-init modules --mode=final --file /tmp/cloud-init.yml
