8321bec570fecc967d15a32c54e50155acf0dc7d
Loren Johnson <lorenjohnson@gmail.com>
2026-07-23 12:26:08 +0200

    docs(wikibase): explain temporary OAuth user ID settings

--- build/wikibase/LocalSettings.d/40_OAuth.php
+++ build/wikibase/LocalSettings.d/40_OAuth.php
@@ -2,6 +2,11 @@
 
 ## OAuth Extension
 wfLoadExtension( 'OAuth' );
+// TODO: OAuth is deprecating $wgMWOAuthSharedUserIDs and will eventually
+// always use the shared-user-ID lookup. Using the local provider preserves
+// WBS's previous single-wiki behavior. When the setting is removed upstream,
+// remove $wgMWOAuthSharedUserIDs and verify whether the explicit local source
+// is still necessary.
 $wgMWOAuthSharedUserIDs = true;
 $wgMWOAuthSharedUserSource = 'local';
 $wgGroupPermissions['sysop']['mwoauthproposeconsumer'] = true;
